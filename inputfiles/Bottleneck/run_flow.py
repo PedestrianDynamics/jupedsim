945b5d533a33091e6838e4ec8886743d5f9508a5
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2012-12-06 22:18:22 +0000

    add logging to run_flow(). flow values are saved in file with the same name as figure (txt, png)

--- inputfiles/Bottleneck/run_flow.py
+++ inputfiles/Bottleneck/run_flow.py
@@ -1,30 +1,29 @@
 #!/usr/bin/python
 import numpy as np
 from xml.dom import minidom
-from sys import argv ,exit
-import os
-import time
+import os, argparse, logging, time
 from os import path, system
 from matplotlib.pyplot import *
-import argparse
-
+from sys import argv ,exit
+#----------------------------
 parser = argparse.ArgumentParser(description='Simulate the bottleneck-scenario with GCFM and calculte the flow with respect to the width of the bottleneck')
 parser.add_argument("-p", "--plt", type=int , choices=xrange(0, 2), required=True, help='Plot and save the results in flow_pngs/')
 parser.add_argument("-s", "--sim" , type=int , choices=xrange(0, 2), required=True, help="Make simulations. Old sim-trajectories will be deleted")
 parser.add_argument("-i" , "--ini" , type=int , choices=xrange(0, 2), required=True, help="Produce ini-files based on the <ini-Bottleneck.xml> file")
+parser.add_argument("-l" , "--log" , type=argparse.FileType('w'), default='log.txt', help="log file (default log.txt)")
 args = parser.parse_args()
 #-------------------- CONTROL VARS ----------------------
 SIM = args.sim # 0: shutdown simulations, 1: make simulations
 MAKE_INI = args.ini # generate new ini-files. 
 PLOT = args.plt # plot or not?
+logfile = args.log.name
 #--------------------------------------------------------
+logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-print 'SIM', SIM
-print 'MAKE_INI', MAKE_INI
-print 'PLOT', PLOT
-
-raw_input("pause")
-
+logging.info('SIM = %d'%SIM)
+logging.info('MAKE_INI = %d'%MAKE_INI)
+logging.info('PLOT = %d'%PLOT)
+logging.info('logfile = \"%s\"'%logfile)
 
 #-------------------- DIRS ------------------------------
 HOME = path.expanduser("~")
@@ -38,7 +37,7 @@ def sh(script):
 	# run bash command
 	system("bash -c '%s'" % script)
 	
-def parse_file(file):
+def parse_file(filename):
 	"""
 	parse trajectories in Travisto-format and output results
 	in the following  format: id    frame    x    y
@@ -47,11 +46,15 @@ def parse_file(file):
 	N: number of pedestrians
 	data: trajectories
 	"""
-	xmldoc = minidom.parse(file)
+	logging.info("parsing <%s>"%filename)
+	try:
+		xmldoc = minidom.parse(filename)
+	except:
+		exit(logging.critical('could not parse file. exit'))
 	N = int(xmldoc.getElementsByTagName('agents')[0].childNodes[0].data)
+
 	fps = int(xmldoc.getElementsByTagName('frameRate')[0].childNodes[0].data)
-	print "Npeds = ", N
-	print "fps = ",fps
+	logging.info ("Npeds = %d, fps = %d"%(N, fps))
 	frames = xmldoc.childNodes[0].getElementsByTagName('frame')
 	data = []
 	for frame in frames:
@@ -76,18 +79,17 @@ def flow(fps, N, data, x0):
 	output:
 	- flow
 	"""
+	logging.info('measure flow')
 	if not isinstance(data, np.ndarray):
-		exit("ERROR: flow accepts data of type <ndarray>")
+		exit(logging.critical("flow() accepts data of type <ndarray>. exit"))
 	peds = np.unique(data[:,0]).astype(int)
 	times = []
 	for ped in peds:
 		d = data[ data[:,0] == ped ]
 		first = min( d[ d[:,2] >= x0 ][:,1] )
 		times.append( first )
-		#print "ped", ped
-		#print "first", first
 	if len(times) < 2:
-		print "WARNING: times=", times, " return 0"
+		logging.warning("Number of pedestrians passing the line is small. return 0")
 		return 0	
 	flow = fps * float(N-1) / ( max(times) - min(times) )
 	return flow
@@ -105,9 +107,9 @@ if __name__ == "__main__":
 		trajfile = TRAJDIR + str(w) + "_" + "TrajBottleneck.xml"
 		inifile =  TRUNK + str(w) + "_ini-Bottleneck.xml"
 		if not path.exists(geofile):
-			exit("ERROR: geofile <%s> does not exist"%geofile)
+			exit(logging.critical("geofile <%s> does not exist"%geofile))
 		if not path.exists(inifile):
-			exit("ERROR: inifile <%s> does not exist"%inifile)
+			exit(logging.critical("inifile <%s> does not exist"%inifile))
 
 		#--------------------- SIMULATION ------------------------	
 		if SIM:
@@ -116,20 +118,27 @@ if __name__ == "__main__":
 				sh(remove)
 			os.chdir(TRUNK) #cd to the simulation directory
 			cmd = "./rebuild.exe --inifile=%s"%inifile
+			logging.info('start simulating ...')
 			sh(cmd) #make simulation
 			os.chdir(CWD) # cd back to the working directory
 			
 		#--------------------- PARSING & FLOW-MEASUREMENT --------
 		if not path.exists(trajfile):
-			exit("ERROR: trajfile <%s> does not exit"%trajfile)
-		print "parsing <%s>"%trajfile
+			exit(logging.critical("trajfile <%s> does not exit"%trajfile))
 		fps, N, traj = parse_file(trajfile)
 		J = flow(fps, N, traj, 6100)
 		flows.append(J)
 	#------------------------------------------------------------------------------	
-	print "flows: ", flows
+	logging.debug("flows: (%s)"%', '.join(map(str, flows)))
+	
 	# ----------------------- PLOT RESULTS ----------------------
 	if len(flows) > 1 and PLOT:
+		timestamp = str( int (time.time() ) )
+		flow_file = "flow_pngs/flow_" + timestamp + ".txt"
+		ff = open(flow_file, "w")
+		logging.info('write flow values in \"%s\"'%flow_file)
+		print >>ff, flows
+		ff.close
 		ms = 10 #marker size
 		plot(widths, flows, "o-", lw = 2, ms = ms, label = "simulation")
 		sey = np.loadtxt("bck-b-scaling/seyfried-j-b.dat")
@@ -142,6 +151,11 @@ if __name__ == "__main__":
 		plot(muel26[:,0], muel26[:,1]/muel26[:,2], "x", ms = ms, label = "Mueller b_g=2.6m")
 		legend(loc='best')
 		grid()
-		figname = "flow_pngs/flow_" + str( int (time.time() ) ) + ".png"
+		figname = "flow_pngs/flow_" + timestamp + ".png"
+		logging.info("saving figure in \"%s\" "%figname)
 		savefig(figname)
-		show()
+		logging.info("svn add %s"%figname)
+		sh("svn add %s"%figname)
+		logging.info("svn add %s"%flow_file)
+		sh("svn add %s"%flow_file)
+		#show()
