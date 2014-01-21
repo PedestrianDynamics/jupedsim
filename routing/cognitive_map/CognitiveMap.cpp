e94bc4029f6a74ecf7a7082cec255d2f9220f72c
David Haensel <david@davidscorner.de>
2014-01-21 15:08:52 +0000

    Added CognitiveMap Skeletons.

---
+++ routing/cognitive_map/CognitiveMap.cpp
@@ -0,0 +1,24 @@
+/*
+ * CognitiveMap.cpp
+ *
+ *  Created on: Jan 2014
+ *      Author: David Haensel
+ */
+
+#include "CognitiveMap.h"
+
+using namespace std;
+
+/**
+ * Constructors & Destructors
+ */
+
+CognitiveMap::CognitiveMap()
+{
+
+}
+
+CognitiveMap::~CognitiveMap()
+{
+
+};
---
+++ routing/cognitive_map/CognitiveMap.h
@@ -0,0 +1,30 @@
+/*
+ * CognitiveMap.h
+ *
+ *  Created on: Jan 2014
+ *      Author: David Haensel
+ */
+
+#ifndef COGNITIVEMAP_H_
+#define COGNITIVEMAP_H_
+
+
+class CognitiveMap {
+
+public:
+	/****************************
+	 * Constructors & Destructors
+	 ****************************/
+
+	CognitiveMap();
+
+	virtual ~CognitiveMap();
+
+
+private:
+
+
+};
+
+
+#endif /* COGNITIVEMAP_H_ */
