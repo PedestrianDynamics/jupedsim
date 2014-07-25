Bug in calculation of cutoff radius
1. Cutoff radius is only calculated for Gompertz not for GCFM
2. New trajectory format (xPos, yPos) --> (x,y).
3. move some python-functions to a separate module (utils.py) to avoid repetition

Utest/test_2/runtest_2.py
@@ -7,8 +7,10 @@ from sys import argv ,exit
 import subprocess, glob
 import multiprocessing
 import matplotlib.pyplot as plt
-import re
-
+import re, sys
+lib_path = os.path.abspath('..')
+sys.path.append(lib_path)
+from utils import *
 #=========================
 testnr = 2
 #========================
@@ -28,55 +30,6 @@ CWD = os.getcwd()
 DIR= os.path.dirname(os.path.realpath(argv[0])) 
 #--------------------------------------------------------
     
-def get_maxtime(filename):
-    """
-    get max sim time
-    """
-    logging.info("parsing <%s>"%filename)
-    try:
-        xmldoc = minidom.parse(filename)
-    except:
-        logging.critical('could not parse file. exit')
-        exit(FAILURE)        
-    maxtime  = float(xmldoc.getElementsByTagName('max_sim_time')[0].firstChild.nodeValue)
-    return maxtime
-
-def parse_file(filename):
-    """
-    parse trajectories in Travisto-format and output results
-    in the following  format: id    frame    x    y
-    (no sorting of the data is performed)
-    returns
-    N: number of pedestrians
-    data: trajectories
-    """
-    logging.info("parsing <%s>"%filename)
-    try:
-        xmldoc = minidom.parse(filename)
-    except:
-        logging.critical('could not parse file. exit')
-        exit(FAILURE)
-    N = int(xmldoc.getElementsByTagName('agents')[0].childNodes[0].data)
-    fps= xmldoc.getElementsByTagName('frameRate')[0].childNodes[0].data #type unicode
-    fps = float(fps)
-    fps = int(fps)
-    print "fps=", fps
-    #fps = int(xmldoc.getElementsByTagName('frameRate')[0].childNodes[0].data)
-    logging.info ("Npeds = %d, fps = %d"%(N, fps))
-    frames = xmldoc.childNodes[0].getElementsByTagName('frame')
-    data = []
-    for frame in frames:
-        frame_number = int(frame.attributes["ID"].value)
-        for agent in frame.getElementsByTagName("agent"):
-            agent_id = int(agent.attributes["ID"].value)
-            x = float(agent.attributes["xPos"].value)
-            y = float(agent.attributes["yPos"].value)
-            data += [agent_id, frame_number, x, y]
-    data = np.array(data).reshape((-1,4))
-    return fps, N, data
-    
-
-
 if __name__ == "__main__":
     if CWD != DIR:
         logging.info("working dir is %s. Change to %s"%(os.getcwd(), DIR))