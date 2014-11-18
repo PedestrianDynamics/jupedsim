dc920435b0913019e6a9d44c4441bd2792ef68fe
Ulrich Kemloh <kemlohulrihc@gmail.com>
2014-11-18 20:49:18 +0100

    fixed osx compilation issue. Added <random> header needed by clang.

--- routing/GlobalRouter.h
+++ routing/GlobalRouter.h
@@ -2,7 +2,7 @@
  * \file        GlobalRouter.h
  * \date        Dec 15, 2010
  * \version     v0.5
- * \copyright   <2009-2014> Forschungszentrum J�lich GmbH. All rights reserved.
+ * \copyright   <2009-2014> Forschungszentrum J���lich GmbH. All rights reserved.
  *
  * \section License
  * This file is part of JuPedSim.
@@ -47,7 +47,7 @@ class OutputHandler;
 //log output
 extern OutputHandler* Log;
 
-
+#include <random>
 
 class GlobalRouter: public Router {
 
