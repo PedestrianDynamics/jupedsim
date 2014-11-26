34a004cf76efc8042a1a3fd05c07362621d78933
Mohcine Chraibi <m.chraibi@gmail.com>
2014-11-26 16:03:59 +0100

    Resolve some windows header problems
    See issue #56

--- routing/GlobalRouter.h
+++ routing/GlobalRouter.h
@@ -2,7 +2,7 @@
  * \file        GlobalRouter.h
  * \date        Dec 15, 2010
  * \version     v0.5
- * \copyright   <2009-2014> Forschungszentrum J���lich GmbH. All rights reserved.
+ * \copyright   <2009-2014> Forschungszentrum Jülich GmbH. All rights reserved.
  *
  * \section License
  * This file is part of JuPedSim.
