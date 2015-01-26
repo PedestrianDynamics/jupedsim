5dd3b333230eb8cbcc6cac806140ee4845b986e3
Arne Graf <ar.graf.cst@gmail.com>
2015-01-26 15:07:22 +0100

    added HeapTree files to cmakelist

---
+++ math/HeapTree.cpp
@@ -0,0 +1,11 @@
+#include "HeapTree.h"
+
+HeapTree::HeapTree()
+{
+    //ctor
+}
+
+HeapTree::~HeapTree()
+{
+    //dtor
+}
---
+++ math/HeapTree.h
@@ -0,0 +1,14 @@
+#ifndef HEAPTREE_H
+#define HEAPTREE_H
+
+
+class HeapTree
+{
+    public:
+        HeapTree();
+        virtual ~HeapTree();
+    protected:
+    private:
+};
+
+#endif // HEAPTREE_H
