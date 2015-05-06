0b04a3d68a9c47fdb775933a8ce39f99f91db4d8
Oliver Schmidts <o.schmidts@fz-juelich.de>
2015-05-06 10:59:43 +0200

    removing old runtests

--- Utest/test_15/runtest_15.py
+++ Utest/test_15/runtest_15.py
@@ -1,107 +1,43 @@
-#! /usr/bin/env python
-import numpy as np
-import os, argparse, logging, time, sys
-from os import path, system
-from sys import argv ,exit
-import subprocess, glob
-import multiprocessing
+#!/usr/bin/env python
+import os
+import sys
+from sys import *
+import scipy
+import scipy.stats
 import matplotlib.pyplot as plt
-import re
+sys.path.append(os.path.abspath(os.path.dirname(sys.path[0])))
+from JPSRunTest import JPSRunTestDriver
+from utils import *
 
-#=========================
-testnr = int(argv[0].split("_")[-1].split(".")[0])
-#========================
+__author__ = 'Oliver Schmidts'
 
-must_time = 10  # 10 m corridor with 1m/s 
-SUCCESS = 0
-FAILURE = 1
 
-#--------------------------------------------------------
-logfile="log_test_%d.txt"%testnr
-f=open(logfile, "w")
-f.close()
-logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+def runtest15(inifile, trajfile):
+    logsim = "inifiles/log.P0.dat"
+    if not os.path.exists(logsim):
+        logging.critical("logsim <%s> does not exist"%logsim)
+        exit(FAILURE)
+    logging.info("open  <%s> "%logsim)
+    f = open(logsim, "r")
+    for line in f:
+        if line.startswith("Exec"):
+            exec_time = float( line.split()[-1])
 
-#-------------------- DIRS ------------------------------
-HOME = path.expanduser("~")
-DIR= os.path.dirname(os.path.realpath(argv[0]))
-CWD = os.getcwd()
-#--------------------------------------------------------
-    
+        if line.startswith("Evac"):
+            evac_time = float(line.split()[-1])
+
+    f.close()
+
+
+    if evac_time < exec_time:
+        logging.info("%s exits with FAILURE evac_time = %f, exec_time = %f)"%(argv[0], evac_time, exec_time))
+        exit(FAILURE)
+    else:
+        logging.info("evac_time = %f  exec_time = %f)"%(evac_time, exec_time))
+
 
 if __name__ == "__main__":
-    if CWD != DIR:
-        logging.info("working dir is %s. Change to %s"%(os.getcwd(), DIR))
-        os.chdir(DIR)
-
-
-    logging.info("change directory to ..")
-    os.chdir("..")
-    logging.info("call makeini.py with -f %s/master_ini.xml"%DIR)
-    subprocess.call(["python", "makeini.py", "-f", "%s/master_ini.xml"%DIR])
-    os.chdir(DIR)
-    #-------- get directory of the code TRUNK
-    os.chdir("../..")
-    TRUNK = os.getcwd()
-    os.chdir(DIR)
-    lib_path = os.path.abspath("%s/Utest"%TRUNK)
-    sys.path.append(lib_path)
-    from utils import *
-    #----------------------------------------
-    logging.info("change directory back to %s"%DIR)
-
-    geofile = "%s/geometry.xml"%DIR
-    inifiles = glob.glob("inifiles/*.xml")
-    if not path.exists(geofile):
-        logging.critical("geofile <%s> does not exist"%geofile)
-        exit(FAILURE)
-        
-    executable = "%s/bin/jpscore"%TRUNK
-    if not path.exists(executable):
-        logging.critical("executable <%s> does not exist yet."%executable)
-        exit(FAILURE)
-        
-    for inifile in inifiles:
-        if not path.exists(inifile):
-            logging.critical("inifile <%s> does not exist"%inifile)
-            exit(FAILURE)
-        #--------------------- SIMULATION ------------------------  
-        #os.chdir(TRUNK) #cd to the simulation directory      
-        cmd = "%s --inifile=%s"%(executable, inifile)
-        logging.info('start simulating with exe=<%s>'%(cmd))
-        #------------------------------------------------------
-        subprocess.call([executable, "--inifile=%s"%inifile])
-        #------------------------------------------------------
-        logging.info('end simulation ...\n--------------\n')
-        trajfile = "trajectories/traj" + inifile.split("ini")[2]
-        logging.info('trajfile = <%s>'%trajfile)
-        #--------------------- PARSING & FLOW-MEASUREMENT --------
-        if not path.exists(trajfile):
-            logging.critical("trajfile <%s> does not exist"%trajfile)
-            exit(FAILURE)
-
-        logsim = "inifiles/log.P0.dat"
-        if not path.exists(logsim):
-            logging.critical("logsim <%s> does not exist"%logsim)
-            exit(FAILURE)
-    
-        logging.info("open  <%s> "%logsim)
-        f = open(logsim, "r")
-        for line in f:
-            if line.startswith("Exec"):
-                exec_time = float( line.split()[-1])
-
-            if line.startswith("Evac"):
-                evac_time = float(line.split()[-1])
-
-        f.close()
-        
-        
-        if evac_time < exec_time:
-            logging.info("%s exits with FAILURE evac_time = %f, exec_time = %f)"%(argv[0], evac_time, exec_time))
-            exit(FAILURE)
-        else:
-            logging.info("evac_time = %f  exec_time = %f)"%(evac_time, exec_time))
-        
+    test = JPSRunTestDriver(15, argv0=argv[0], testdir=sys.path[0])
+    test.run_test(runtest15)
     logging.info("%s exits with SUCCESS"%(argv[0]))
     exit(SUCCESS)
--- Utest/test_7/runtest_7.py
+++ Utest/test_7/runtest_7.py
@@ -1,31 +1,46 @@
-#! /usr/bin/env python
-import numpy as np
-from xml.dom import minidom
-import os, argparse, logging, time
-from os import path, system
-from sys import argv ,exit
-import subprocess, glob
-import multiprocessing
-import matplotlib.pyplot as plt
-import re, sys
+#!/usr/bin/env python
+import os
+import sys
+from sys import *
+sys.path.append(os.path.abspath(os.path.dirname(sys.path[0])))
+from JPSRunTest import JPSRunTestDriver
+from utils import *
 
-#=========================
-testnr = 7
-#========================
+__author__ = 'Oliver Schmidts'
 
-SUCCESS = 0
-FAILURE = 1
-#--------------------------------------------------------
-logfile="log_test_%d.txt"%testnr
-f=open(logfile, "w")
-f.close()
-logging.basicConfig(filename=logfile, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-#-------------------- DIRS ------------------------------
-HOME = path.expanduser("~")
-CWD = os.getcwd()
-DIR = os.path.dirname(os.path.realpath(argv[0]))
-#--------------------------------------------------------
+def runtest7(inifile, trajfile):
+    fps, N, traj = parse_file(trajfile)
+    failure = False
+    group_1 = [1, 2, 3]
+    group_2 = [4, 6, 5]
+    e1 = [26, 1, 2] # y, x1, x2
+    e2 = [18, 1, 2] # x, y1, y2
+    for ped in group_1:
+        traj1 = traj[ traj[:,0] == ped ]
+        x = traj1[:,2]
+        y = traj1[:,3]
+        if not PassedLineY(traj1, e1):
+            logging.critical("ped %d did not exit from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e1[1], e1[0], e1[2], e1[0]))
+            failure = True
+        else:
+            logging.info("ped %d  exits from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e1[1], e1[0], e1[2], e1[0]))
+
+    for ped in group_2:
+        traj1 = traj[ traj[:,0] == ped ]
+        x = traj1[:,2]
+        y = traj1[:,3]
+        if not PassedLineX(traj1, e2):
+            logging.critical("ped %d did not exit from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e2[0], e2[1], e2[0], e2[2]))
+            failure = True
+        else:
+            logging.info("ped %d  exits from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e2[0], e2[1], e2[0], e2[2]))
+
+    if failure:
+        logging.critical("%s exists with failure!"%argv[0])
+        exit(FAILURE)
+
+
 def PassedLineX(p, exit):
     """
     check if pedestrian (given by matrix p) passed the vertical line x, [y1, y2] y1<y2
@@ -34,6 +49,8 @@ def PassedLineX(p, exit):
     y1 = exit[1]
     y2 = exit[2]
     return any(p[:,2] <= x) & any(p[:,2] >= x) & any(p[:,3] >= y1) & any(p[:,3] <= y2)
+
+
 def PassedLineY(p, exit):
     """
     check if pedestrian (given by matrix p) passed the horizontal line y, [x1, x2] x1<x2
@@ -42,91 +59,10 @@ def PassedLineY(p, exit):
     x1 = exit[1]
     x2 = exit[2]
     return any(p[:,3] <= y) & any(p[:,3] >= y) & any(p[:,2] >= x1) & any(p[:,2] <= x2)
-    
-    
 
-failure = 0
+
 if __name__ == "__main__":
-    if CWD != DIR:
-        logging.info("working dir is %s. Change to %s"%(os.getcwd(), DIR))
-        os.chdir(DIR)
-
-    logging.info("change directory to ..")
-    os.chdir("..")
-    logging.info("call makeini.py with -f %s/master_ini.xml"%DIR)
-    subprocess.call(["python", "makeini.py", "-f", "%s/master_ini.xml"%DIR])
-    os.chdir(DIR)
-    #-------- get directory of the code TRUNK
-    os.chdir("../..")
-    TRUNK = os.getcwd()
-    os.chdir(DIR)
-    lib_path = os.path.abspath("%s/Utest"%TRUNK)
-    sys.path.append(lib_path)
-    from utils import *
-    #----------------------------------------
-    logging.info("change directory back to %s"%DIR)
-    geofile = "%s/geometry.xml"%DIR
-    logging.info("geofile <%s>"%geofile)
-    inifiles = glob.glob("inifiles/*.xml")
-    if not path.exists(geofile):
-        logging.critical("geofile <%s> does not exist"%geofile)
-        exit(FAILURE)
-        
-    executable = "%s/bin/jpscore"%TRUNK
-    if not path.exists(executable):
-        logging.critical("executable <%s> does not exist yet."%executable)
-        exit(FAILURE)
-        
-    for inifile in inifiles:
-        if not path.exists(inifile):
-            logging.critical("inifile <%s> does not exist"%inifile)
-            exit(FAILURE)
-        #--------------------- SIMULATION ------------------------  
-        #os.chdir(TRUNK) #cd to the simulation directory      
-        cmd = "%s --inifile=%s"%(executable, inifile)
-        logging.info('start simulating with exe=<%s>'%(cmd))
-        #------------------------------------------------------
-        subprocess.call([executable, "--inifile=%s"%inifile])
-        #------------------------------------------------------
-        logging.info('end simulation ...\n--------------\n')
-        trajfile = "trajectories/traj" + inifile.split("ini")[2]
-        logging.info('trajfile = <%s>'%trajfile)
-        #--------------------- PARSING & TEST LOGIC --------
-        if not path.exists(trajfile):
-            logging.critical("trajfile <%s> does not exist"%trajfile)
-            exit(FAILURE)
-  
-        fps, N, traj = parse_file(trajfile)
-        group_1 = [1, 2, 3]
-        group_2 = [4, 6, 5]
-        e1 = [26, 1, 2] # y, x1, x2
-        e2 = [18, 1, 2] # x, y1, y2
-        for ped in group_1:
-            traj1 = traj[ traj[:,0] == ped ]
-            x = traj1[:,2]
-            y = traj1[:,3]
-            if not PassedLineY(traj1, e1):
-                logging.critical("ped %d did not exit from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e1[1], e1[0], e1[2], e1[0]))
-                failure = 1
-            else:
-                logging.info("ped %d  exits from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e1[1], e1[0], e1[2], e1[0]))
-
-        for ped in group_2:
-            traj1 = traj[ traj[:,0] == ped ]
-            x = traj1[:,2]
-            y = traj1[:,3]
-            if not PassedLineX(traj1, e2):
-                logging.critical("ped %d did not exit from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e2[0], e2[1], e2[0], e2[2]))
-                failure = 1
-            else:
-                logging.info("ped %d  exits from Exit (%1.2f, %1.2f) | (%1.2f, %1.2f)"%(ped, e2[0], e2[1], e2[0], e2[2]))
-
-         
-        if failure:
-            logging.critical("%s exists with failure!"%argv[0]) 
-            exit(FAILURE)
-
-#----- for inifiles    
-    logging.info("%s exists with success!"%argv[0]) 
+    test = JPSRunTestDriver(7, argv0=argv[0], testdir=sys.path[0])
+    test.run_test(testfunction=runtest7)
+    logging.info("%s exits with SUCCESS" % (argv[0]))
     exit(SUCCESS)
-
