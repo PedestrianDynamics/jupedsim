39a544249d884a2aed3c9c368a629cf887fa6beb
Weichen <w.liao@fz-juelich.de>
2015-07-20 16:14:28 +0200

    Multiple steady states
    Record all the detected steady states.
    Calculate the ratio of each steady state to all states.
    Calculate the mean density/speed and the corresponding standard deviation in each steady state.

--- scripts/SteadyState.py
+++ scripts/SteadyState.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import scipy.stats
 from numpy import *
@@ -68,7 +69,7 @@ filepath = input_file[:(len(input_file)-count)]
 filename = input_file[-count:]
 print('file path = ', filepath)
 print('file name = ', filename)
-
+    
 # calculate ref_mean, ref_std, ref_acf for rho
 ref_rho = data
 ref_rho = ref_rho[ ref_rho[:,0] >= ref_rho_start ]
@@ -250,7 +251,7 @@ print('v_theta = %.0f'%(v_theta))
 # calculate statistics rho
 statistics_rho = data
 statistics_rho = statistics_rho[:,1]
-file_rho_s = open('%s/cusum_rho_%s.txt'%(filepath, filename), 'w')
+file_rho_s = open('%s/cusum_rho_%s.txt'%(filepath,filename), 'w')
 file_rho_s.write('# frame s \n')
 rho_s_frame = 0
 rho_s = s_max
@@ -264,7 +265,7 @@ file_rho_s.close()
 # calculate statistics v
 statistics_v = data
 statistics_v = statistics_v[:,2]
-file_v_s = open('%s/cusum_v_%s.txt'%(filepath, filename), 'w')
+file_v_s = open('%s/cusum_v_%s.txt'%(filepath,filename), 'w')
 file_v_s.write('# frame s \n')
 v_s_frame = 0
 v_s = s_max
@@ -276,81 +277,92 @@ for i in statistics_v:
 file_v_s.close()
 
 # choose steady state rho
-statistics_rho = loadtxt('%s/cusum_rho_%s.txt'%(filepath, filename))
+statistics_rho = loadtxt('%s/cusum_rho_%s.txt'%(filepath,filename))
+ss_rho = open('%s/SteadyState_rho_%s.txt'%(filepath,filename), 'w')
+ss_rho.write('# start end ratio mean std \n')
 steady_rho = statistics_rho[ statistics_rho[:,1] < rho_theta ]
-steady_rho_start_list = []
-steady_rho_end_list = []
 steady_rho_start = min(steady_rho[:,0]) - (s_max-rho_theta)
-steady_rho_start_list.extend([steady_rho_start])
 for i in arange(1, len(steady_rho), 1):
     if steady_rho[i,0] - steady_rho[i-1,0] != 1:
         steady_rho_end = steady_rho[i-1,0] - rho_theta
-        steady_rho_end_list.extend([steady_rho_end])
-        steady_rho_start = steady_rho[i,0] - (s_max-rho_theta)
-        steady_rho_start_list.extend([steady_rho_start])
-if len(steady_rho_start_list) != len(steady_rho_end_list):
-    steady_rho_end = max(steady_rho[:,0]) - rho_theta
-    steady_rho_end_list.extend([steady_rho_end])
-steady_rho_list = []
-steady_rho_list.append(steady_rho_start_list)
-steady_rho_list.append(steady_rho_end_list)
-steady_rho_array = array(steady_rho_list)
-steady_rho_array = steady_rho_array.T
-savetxt('%s/SteadyState_rho_%s.txt'%(filepath, filename), steady_rho_array, fmt='%d\t%d', header='start end', newline='\r\n')
-print('steady state of rho is: \n', steady_rho_array)
+        if steady_rho_start < steady_rho_end:
+            rho_data = data
+            rho_data = rho_data[ rho_data[:,0] >= steady_rho_start ]
+            rho_data = rho_data[ rho_data[:,0] <= steady_rho_end ]
+            rho_data_ratio = len(rho_data[:,0]) / len(data[:,0]) * 100
+            rho_data_mean = mean(rho_data[:,1])
+            rho_data_std = std(rho_data[:,1])
+            ss_rho.write('%.0f %.0f %.2f %.4f %.4f \n'%(steady_rho_start, steady_rho_end, rho_data_ratio, rho_data_mean, rho_data_std))
+        steady_rho_start = steady_rho[i,0] - (s_max - rho_theta)
+steady_rho_end = max(steady_rho[:,0]) - rho_theta
+if steady_rho_start < steady_rho_end:
+            rho_data = data
+            rho_data = rho_data[ rho_data[:,0] >= steady_rho_start ]
+            rho_data = rho_data[ rho_data[:,0] <= steady_rho_end ]
+            rho_data_ratio = len(rho_data[:,0]) / len(data[:,0]) * 100
+            rho_data_mean = mean(rho_data[:,1])
+            rho_data_std = std(rho_data[:,1])
+            ss_rho.write('%.0f %.0f %.2f %.4f %.4f \n'%(steady_rho_start, steady_rho_end, rho_data_ratio, rho_data_mean, rho_data_std))
+ss_rho.close()
+info_rho = loadtxt('%s/SteadyState_rho_%s.txt'%(filepath,filename))
+if info_rho.shape == (5,):
+    temp = []
+    temp.append(info_rho)
+    info_rho = array(temp)
+print('steady state of rho is: \n', info_rho)
 
 # choose steady state v
-statistics_v = loadtxt('%s/cusum_v_%s.txt'%(filepath, filename))
+statistics_v = loadtxt('%s/cusum_v_%s.txt'%(filepath,filename))
+ss_v = open('%s/SteadyState_v_%s.txt'%(filepath,filename), 'w')
+ss_v.write('# start end ratio mean std \n')
 steady_v = statistics_v[ statistics_v[:,1] < v_theta ]
-steady_v_start_list = []
-steady_v_end_list = []
 steady_v_start = min(steady_v[:,0]) - (s_max-v_theta)
-steady_v_start_list.extend([steady_v_start])
 for i in arange(1, len(steady_v), 1):
     if steady_v[i,0] - steady_v[i-1,0] != 1:
         steady_v_end = steady_v[i-1,0] - v_theta
-        steady_v_end_list.extend([steady_v_end])
-        steady_v_start = steady_v[i,0] - (s_max-v_theta)
-        steady_v_start_list.extend([steady_v_start])
-if len(steady_v_start_list) != len(steady_v_end_list):
-    steady_v_end = max(steady_v[:,0]) - v_theta
-    steady_v_end_list.extend([steady_v_end])
-steady_v_list =[]
-steady_v_list.append(steady_v_start_list)
-steady_v_list.append(steady_v_end_list)
-steady_v_array = array(steady_v_list)
-steady_v_array = steady_v_array.T
-savetxt('%s/SteadyState_v_%s.txt'%(filepath, filename), steady_v_array, fmt='%d\t%d', header='start end', newline='\r\n')
-print('steady state of v is: \n', steady_v_array)
+        if steady_v_start < steady_v_end:
+            v_data = data
+            v_data = v_data[ v_data[:,0] >= steady_v_start ]
+            v_data = v_data[ v_data[:,0] <= steady_v_end ]
+            v_data_ratio = len(v_data[:,0]) / len(data[:,0]) * 100
+            v_data_mean = mean(v_data[:,2])
+            v_data_std = std(v_data[:,2])
+            ss_v.write('%.0f %.0f %.2f %.4f %.4f \n'%(steady_v_start, steady_v_end, v_data_ratio, v_data_mean, v_data_std))
+        steady_v_start = steady_v[i,0] - (s_max - v_theta)
+steady_v_end = max(steady_v[:,0]) - v_theta
+if steady_v_start < steady_v_end:
+            v_data = data
+            v_data = v_data[ v_data[:,0] >= steady_v_start ]
+            v_data = v_data[ v_data[:,0] <= steady_v_end ]
+            v_data_ratio = len(v_data[:,0]) / len(data[:,0]) * 100
+            v_data_mean = mean(v_data[:,2])
+            v_data_std = std(v_data[:,2])
+            ss_v.write('%.0f %.0f %.2f %.4f %.4f \n'%(steady_v_start, steady_v_end, v_data_ratio, v_data_mean, v_data_std))
+ss_v.close()
+info_v = loadtxt('%s/SteadyState_v_%s.txt'%(filepath,filename))
+if info_v.shape == (5,):
+    temp = []
+    temp.append(info_v)
+    info_v = array(temp)
+print('steady state of v is: \n', info_v)
 
 # calculate steady state
-steady_start_list = []
-steady_end_list = []
-for i in range(len(steady_v_array[:,0])):
-        for j in range(len(steady_rho_array[:,0])):
-            if steady_v_array[i,0] <= steady_rho_array[j,0]:
-                if steady_v_array[i,1] > steady_rho_array[j,0] and steady_v_array[i,1] < steady_rho_array[j,1]:
-                    steady_start = steady_rho_array[j,0]
-                    steady_end = steady_v_array[j,1]
-                if steady_v_array[i,1] >= steady_rho_array[j,1]:
-                    steady_start = steady_rho_array[j,0]
-                    steady_end = steady_rho_array[j,1]
-            if steady_v_array[i,0] > steady_rho_array[j,0] and steady_v_array[i,0] <= steady_rho_array[j,1]:
-                if steady_v_array[i,1] <= steady_rho_array[j,1]:
-                    steady_start = steady_v_array[j,0]
-                    steady_end = steady_v_array[j,1]
-                if steady_v_array[i,1] > steady_rho_array[j,1]:
-                    steady_start = steady_v_array[j,0]
-                    steady_end = steady_rho_array[j,1]
-            steady_start_list.extend([steady_start])
-            steady_end_list.extend([steady_end])
-steady_list =[]
-steady_list.append(steady_start_list)
-steady_list.append(steady_end_list)
-steady_array = array(steady_list)
-steady_array = steady_array.T
-savetxt('%s/SteadyState_%s.txt'%(filepath, filename), steady_array, fmt='%d\t%d', header='start end', newline='\r\n')
-print('final steady state is: \n', steady_array)
+ss = open('%s/SteadyState_%s.txt'%(filepath,filename), 'w')
+ss.write('# start end ratio \n')
+for i in range(len(info_rho[:,0])):
+    for j in range(len(info_v[:,0])):
+        mix_start = max(info_rho[i,0], info_v[j,0])
+        mix_end = min(info_rho[i,1], info_v[j,1])
+        if mix_start < mix_end:
+            ss_data_ratio = (mix_end - mix_start) / len(data[:,0]) * 100
+            ss.write('%.0f %.0f %.2f \n'%(mix_start, mix_end, ss_data_ratio))
+ss.close()
+info = loadtxt('%s/SteadyState_%s.txt'%(filepath,filename))
+if info.shape == (3,):
+    temp = []
+    temp.append(info)
+    info = array(temp)
+print('final steady state is: \n', info)
 
 if plotfigs == 'no':
     print('No figures are plotted!')
@@ -370,6 +382,7 @@ else:
     plt.ylim(-10, 120)
     plt.legend(numpoints=2, ncol=1, loc=1, fontsize=20)
     plt.savefig('%s/cusum_rho_%s.png'%(filepath, filename))
+    plt.close()
     
     # plot cusum v
     fig = plt.figure(figsize=(11, 10), dpi=100)
@@ -384,18 +397,19 @@ else:
     plt.ylim(-10, 120)
     plt.legend(numpoints=2, ncol=1, loc=1, fontsize=20)
     plt.savefig('%s/cusum_v_%s.png'%(filepath, filename))
+    plt.close()
     
     # plot steady rho
     fig = plt.figure(figsize=(11,10), dpi=100)
     ax = fig.add_subplot(111)
     limit = (int((statistics_rho[-1,0]/frame)/10)+1)*10    
     plt.plot((data[:,0]-data[0,0])/frame, data[:,1], 'b-', lw=2)
-    plt.plot([ref_rho_start/frame,ref_rho_start/frame], [0,50], 'g--', lw=2, label='reference process')
+    plt.plot([ref_rho_start/frame,ref_rho_start/frame], [0,50], 'g--', lw=2, label='reference')
     plt.plot([ref_rho_end/frame,ref_rho_end/frame], [0,50], 'g--', lw=2)
-    for i in range(len(steady_rho_array[:,0])):
-        ax.add_patch(mpatches.Polygon([[steady_rho_array[i,0]/frame,0],[steady_rho_array[i,1]/frame,0],[steady_rho_array[i,1]/frame,50],[steady_rho_array[i,0]/frame,50]], closed=True, fill=False, color='r', hatch='/', label='steady state (rho)'))
-    for i in range(len(steady_array[:,0])):
-        ax.add_patch(mpatches.Polygon([[steady_array[i,0]/frame,0],[steady_array[i,1]/frame,0],[steady_array[i,1]/frame,50],[steady_array[i,0]/frame,50]], closed=True, fill=True, color='y', alpha=0.2, label='steady state (final)'))
+    for i in range(len(info_rho[:,0])):
+        ax.add_patch(mpatches.Polygon([[info_rho[i,0]/frame,0],[info_rho[i,1]/frame,0],[info_rho[i,1]/frame,50],[info_rho[i,0]/frame,50]], closed=True, fill=False, color='r', hatch='/', label='steady (rho)'))
+    for i in range(len(info[:,0])):
+        ax.add_patch(mpatches.Polygon([[info[i,0]/frame,0],[info[i,1]/frame,0],[info[i,1]/frame,50],[info[i,0]/frame,50]], closed=True, fill=True, color='y', alpha=0.2, label='steady (final)'))
     plt.xlabel('t [s]', fontsize=25)
     plt.ylabel(r'$\rho$ [m$^{-2}$]', fontsize=25)
     plt.xticks(fontsize=20)
@@ -403,26 +417,31 @@ else:
     plt.xlim(0, limit)
     plt.ylim(0, int(max(data[:,1]))+2)
     plt.legend(numpoints=1, ncol=1, loc=1, fontsize=20)
-    plt.savefig('%s/SteadyState_rho_%s.png'%(filepath, filename))
+    plt.savefig('%s/SteadyState_rho_%s.png'%(filepath,filename))
+    plt.close()
     
     # plot steady v
     fig = plt.figure(figsize=(11,10), dpi=100)
     ax = fig.add_subplot(111)
     limit = (int((statistics_v[-1,0]/frame)/10)+1)*10    
     plt.plot((data[:,0]-data[0,0])/frame, data[:,2], 'b-', lw=2)
-    plt.plot([ref_v_start/frame,ref_v_start/frame], [0,50], 'g--', lw=2, label='reference process')
+    plt.plot([ref_v_start/frame,ref_v_start/frame], [0,50], 'g--', lw=2, label='reference')
     plt.plot([ref_v_end/frame,ref_v_end/frame], [0,50], 'g--', lw=2)
-    for i in range(len(steady_v_array[:,0])):
-        ax.add_patch(mpatches.Polygon([[steady_v_array[i,0]/frame,0],[steady_v_array[i,1]/frame,0],[steady_v_array[i,1]/frame,50],[steady_v_array[i,0]/frame,50]], closed=True, fill=False, color='r', hatch='/', label='steady state (v)'))
-    for i in range(len(steady_array[:,0])):
-        ax.add_patch(mpatches.Polygon([[steady_array[i,0]/frame,0],[steady_array[i,1]/frame,0],[steady_array[i,1]/frame,50],[steady_array[i,0]/frame,50]], closed=True, fill=True, color='y', alpha=0.2, label='steady state (final)'))
-        plt.xlabel('t [s]', fontsize=25)
-    plt.ylabel(r'$v$ [m$\cdot$s$^{-1}$]', fontsize=25)
+    for i in range(len(info_v[:,0])):
+        ax.add_patch(mpatches.Polygon([[info_v[i,0]/frame,0],[info_v[i,1]/frame,0],[info_v[i,1]/frame,50],[info_v[i,0]/frame,50]], closed=True, fill=False, color='r', hatch='/', label='steady (rho)'))
+    for i in range(len(info[:,0])):
+        ax.add_patch(mpatches.Polygon([[info[i,0]/frame,0],[info[i,1]/frame,0],[info[i,1]/frame,50],[info[i,0]/frame,50]], closed=True, fill=True, color='y', alpha=0.2, label='steady (final)'))
+    plt.xlabel('t [s]', fontsize=25)
+    plt.ylabel(r'v [m$\cdot$s$^{-1}$]', fontsize=25)
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
     plt.xlim(0, limit)
     plt.ylim(0, int(max(data[:,2]))+1)
     plt.legend(numpoints=1, ncol=1, loc=1, fontsize=20)
-    plt.savefig('%s/SteadyState_v_%s.png'%(filepath, filename))
+    plt.savefig('%s/SteadyState_v_%s.png'%(filepath,filename))
+    plt.close()
+
+os.remove('%s/cusum_rho_%s.txt'%(filepath,filename))
+os.remove('%s/cusum_v_%s.txt'%(filepath,filename))
 
 print('Steady state detected successfully!')
