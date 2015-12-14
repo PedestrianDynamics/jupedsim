afe05a641d31964b44b9160371c288f0b2a3fcf1
junzhang <ju.zhang@fz-juelich.de>
2015-12-14 11:24:44 +0100

    Issue #38 is resolved. Now when the file paths include blank, it still works on windows system.

--- Analysis.cpp
+++ Analysis.cpp
@@ -378,7 +378,7 @@ int Analysis::RunAnalysis(const string& filename, const string& path)
                     Log->Write("INFO:\tSuccess with Method C using measurement area id %d!\n",_areaForMethod_C[i]->_id);
              	   if(_plotTimeseriesC)
  					 {
- 					 string parameters_Timeseries="python "+_scriptsLocation+"/_Plot_timeseries_rho_v.py -p \""+ _projectRootDir+VORO_LOCATION + "\" -n "+filename+
+ 					 string parameters_Timeseries="python \""+_scriptsLocation+"/_Plot_timeseries_rho_v.py\" -p \""+ _projectRootDir+VORO_LOCATION + "\" -n "+filename+
  								" -f "+boost::lexical_cast<std::string>(data.GetFps());
  					  int res=system(parameters_Timeseries.c_str());
  					  Log->Write("INFO:\t time series result: %d ",res);
@@ -418,7 +418,7 @@ int Analysis::RunAnalysis(const string& filename, const string& path)
             	   Log->Write("INFO:\tSuccess with Method D using measurement area id %d!\n",_areaForMethod_D[i]->_id);
             	   if(_plotTimeseriesD)
 					 {
-					 string parameters_Timeseries="python "+_scriptsLocation+"/_Plot_timeseries_rho_v.py -p \""+ _projectRootDir+VORO_LOCATION + "\" -n "+filename+
+					 string parameters_Timeseries="python \""+_scriptsLocation+"/_Plot_timeseries_rho_v.py\" -p \""+ _projectRootDir+VORO_LOCATION + "\" -n "+filename+
 								" -f "+boost::lexical_cast<std::string>(data.GetFps());
 					  int res=system(parameters_Timeseries.c_str());
 					  Log->Write("INFO:\t time series result: %d ",res);
