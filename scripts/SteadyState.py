1919d9f4a8eaa4126a872eb798d7d5af768c5ae4
caoshuchao <s.cao@fz-juelich.de>
2016-02-03 14:30:23 +0100

    The original output of steadystate.py is the lines of files, not the real frame. Now it has been changed. Fix #46

--- scripts/SteadyState.py
+++ scripts/SteadyState.py
@@ -33,7 +33,7 @@ def func_b(i, k):
 
 def getParserArgs():
     parser = argparse.ArgumentParser(description='Combine French data to one file')
-    parser.add_argument("-v", "--version", help='JuPedSim  Version 0.7  Detection of Steady State')
+    parser.add_argument("-v", "--version", help='JuPedSim  Version 0.8  Detection of Steady State')
     parser.add_argument("-f", "--input_file", default="E:/GitLab/jpsreport/demos/SteadyState/rho_v_Voronoi_traj_AO_b240.txt_id_1.dat", help='give the location and the name of the input file')    
     parser.add_argument("-rs", "--reference_rho_start", type=int, default=240, help='give the start frame of the reference process in density')
     parser.add_argument("-re", "--reference_rho_end", type=int, default=640, help='give the end frame of the reference process in density')
@@ -56,6 +56,7 @@ if __name__ == '__main__':
 # input data
 data = loadtxt('%s'%(input_file))
 data = data[ data[:,1] != 0 ]
+minframe = data[0,0]
 data[:,0] = data[:,0] - data[0,0]
 
 # filepath and filename
@@ -72,8 +73,8 @@ print('file name = ', filename)
     
 # calculate ref_mean, ref_std, ref_acf for rho
 ref_rho = data
-ref_rho = ref_rho[ ref_rho[:,0] >= ref_rho_start ]
-ref_rho = ref_rho[ ref_rho[:,0] <= ref_rho_end ]
+ref_rho = ref_rho[ ref_rho[:,0] >= ref_rho_start - minframe]
+ref_rho = ref_rho[ ref_rho[:,0] <= ref_rho_end - minframe]
 ref_rho = ref_rho[:,1]
 ref_rho_mean = mean(ref_rho)
 ref_rho_std = std(ref_rho)
@@ -84,8 +85,8 @@ ref_rho_acf = ref_rho_acf[0]
 
 # calculate ref_mean, ref_std, ref_acf for v
 ref_v = data
-ref_v = ref_v[ ref_v[:,0] >= ref_v_start ]
-ref_v = ref_v[ ref_v[:,0] <= ref_v_end ]
+ref_v = ref_v[ ref_v[:,0] >= ref_v_start - minframe]
+ref_v = ref_v[ ref_v[:,0] <= ref_v_end - minframe]
 ref_v = ref_v[:,2]
 ref_v_mean = mean(ref_v)
 ref_v_std = std(ref_v)
@@ -253,7 +254,7 @@ statistics_rho = data
 statistics_rho = statistics_rho[:,1]
 file_rho_s = open('%s/cusum_rho_%s.txt'%(filepath,filename), 'w')
 file_rho_s.write('# frame s \n')
-rho_s_frame = 0
+rho_s_frame = minframe
 rho_s = s_max
 file_rho_s.write('%.0f %.4f \n'%(rho_s_frame, rho_s))
 for i in statistics_rho:
@@ -267,7 +268,7 @@ statistics_v = data
 statistics_v = statistics_v[:,2]
 file_v_s = open('%s/cusum_v_%s.txt'%(filepath,filename), 'w')
 file_v_s.write('# frame s \n')
-v_s_frame = 0
+v_s_frame = minframe
 v_s = s_max
 file_v_s.write('%.0f %.4f \n'%(v_s_frame, v_s))
 for i in statistics_v:
@@ -403,7 +404,7 @@ else:
     fig = plt.figure(figsize=(11,10), dpi=100)
     ax = fig.add_subplot(111)
     limit = (int((statistics_rho[-1,0]/frame)/10)+1)*10    
-    plt.plot((data[:,0]-data[0,0])/frame, data[:,1], 'b-', lw=2)
+    plt.plot((data[:,0]+minframe)/frame, data[:,1], 'b-', lw=2)
     plt.plot([ref_rho_start/frame,ref_rho_start/frame], [0,50], 'g--', lw=2, label='reference')
     plt.plot([ref_rho_end/frame,ref_rho_end/frame], [0,50], 'g--', lw=2)
     for i in range(len(info_rho[:,0])):
@@ -424,7 +425,7 @@ else:
     fig = plt.figure(figsize=(11,10), dpi=100)
     ax = fig.add_subplot(111)
     limit = (int((statistics_v[-1,0]/frame)/10)+1)*10    
-    plt.plot((data[:,0]-data[0,0])/frame, data[:,2], 'b-', lw=2)
+    plt.plot((data[:,0]+minframe)/frame, data[:,2], 'b-', lw=2)
     plt.plot([ref_v_start/frame,ref_v_start/frame], [0,50], 'g--', lw=2, label='reference')
     plt.plot([ref_v_end/frame,ref_v_end/frame], [0,50], 'g--', lw=2)
     for i in range(len(info_v[:,0])):
