973badbed7e678fbb689530373ebd1ac32252b9b
Erik Andresen <e.andresen@uni-wuppertal.de>
2016-03-22 15:46:37 +0100

    Change supported old version to 0.7

--- general/Macros.h
+++ general/Macros.h
@@ -59,7 +59,7 @@
 #define _SIMULATOR 1
 //#define _USE_PROTOCOL_BUFFER 1
 
-#define JPS_OLD_VERSION "0.5" // this version is still supported
+#define JPS_OLD_VERSION "0.7" // this version is still supported
 #define JPS_VERSION_MINOR "8"
 #define JPS_VERSION_MAJOR "0"
 
