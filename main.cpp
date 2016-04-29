6662f6714c16e7d0035c14f10f31b10ac597d537
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2016-04-29 09:35:02 +0200

    change version number of main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /**
  * \file        main.cpp
  * \date        Jan 15, 2013
- * \version     v0.7
+ * \version     v0.8
  * \copyright   <2009-2015> Forschungszentrum Jülich GmbH. All rights reserved.
  *
  * \section License
