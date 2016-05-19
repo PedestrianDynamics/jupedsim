b40adf53b6c7b2e24e9e1fadd00c84947e1380af
Erik Andresen <e.andresen@uni-wuppertal.de>
2016-05-19 10:50:31 +0200

    undo some changing for test purposes

--- JPSfire/generic/FDSMesh.cpp
+++ JPSfire/generic/FDSMesh.cpp
@@ -170,15 +170,15 @@ void FDSMesh::ReadMatrix(std::string line, std::ifstream &pFile)
     std::vector<std::string> strVec;
     while (std::getline(pFile, line)) {
         n = 0;
-        split2(line, ',', strVec);
-        for (auto elem : strVec)
+        strVec = split2(line, ',', strVec);
+        for (auto &elem : strVec)
         {
             //std::cout << elem << " col " << n  << " line " << m << std::endl;
-//            if (elem=="nan")
-//            {
-//                Log->Write("ERROR: Mesh values consist of nan!");
-//                exit(EXIT_FAILURE);
-//            }
+            if (elem=="nan")
+            {
+                Log->Write("ERROR: Mesh values consist of nan!");
+                exit(EXIT_FAILURE);
+            }
             _matrix[m][n].SetValue(std::stod(elem));
             ++n;
         }
