f8b7b7a23bcbb8f35629efc22e13dabbe3de933c
f.mack <mack.fabi@t-online.de>
2016-08-24 11:20:49 +0200

    remove double entries from vector (rooms, doors)

--- routing/ffRouter.cpp
+++ routing/ffRouter.cpp
@@ -563,12 +563,12 @@ bool FFRouter::ReInit()
 }
 
 void FFRouter::PrepareForSimulation(Building* building) {
+     std::vector<pair<int, int>> roomsDoorsVector;
+     roomsDoorsVector.clear();
      // @todo f.mack parallelize
 #pragma omp single
      {
-          // collects all <subroom, door> pairs needed using _pathsMatrix
-          std::vector<pair<int, int>> subroomsDoorsVector;
-          subroomsDoorsVector.clear();
+          // collects all <room, door> pairs needed using _pathsMatrix
           for (auto ped : building->GetAllPedestrians()) {
                int finalDoor = _finalDoors.at(ped->GetID());
                int finalDoorID = _CroTrByUID.at(finalDoor)->GetID();
@@ -581,46 +581,62 @@ void FFRouter::PrepareForSimulation(Building* building) {
                } else {
                     finalDoor = goalToLineUIDmap.at(ped->GetFinalDestination());
                }*/
-               int subroomUID = ped->GetSubRoomUID();
-               auto subroom = building->GetSubRoomByUID(subroomUID);
+               int roomID = ped->GetRoomID();
+               auto room = building->GetRoom(roomID);
                int doorUID = ped->GetNextDestination();
                do {
-                    Log->Write("PrepareForSimulation: ped %d needs ff in subroomUID %d (ID %d) to doorUID %d (ID %d)",
-                          ped->GetID(), subroom->GetUID(), subroom->GetSubRoomID(), doorUID, _CroTrByUID[doorUID]->GetID());
-                    if (_CroTrByUID[doorUID]->GetSubRoom1() &&
-                        _CroTrByUID[doorUID]->GetSubRoom1()->GetUID() != subroom->GetUID() &&
-                        _CroTrByUID[doorUID]->GetSubRoom2() &&
-                        _CroTrByUID[doorUID]->GetSubRoom2()->GetUID() != subroom->GetUID()) {
-                         Log->Write(
-                                 "ERROR in PrepareForSimulation: ped %d want to leave subroomUID %d (ID %d) through doorUID %d (ID %d), but this subroom does not belong to this door.",
-                                 ped->GetID(), subroom->GetUID(), subroom->GetSubRoomID(), doorUID, _CroTrByUID[doorUID]->GetID());
+                    Log->Write("PrepareForSimulation: ped %d needs ff in roomID %d to doorUID %d (ID %d)",
+                          ped->GetID(), room->GetID(), doorUID, _CroTrByUID[doorUID]->GetID());
+                    if (_CroTrByUID[doorUID]->GetRoom1() &&
+                        _CroTrByUID[doorUID]->GetRoom1()->GetID() != room->GetID()) {
+                         if (auto tr = dynamic_cast<Transition*>(_CroTrByUID[doorUID])) {
+                              if (tr->GetRoom2() && tr->GetRoom2()->GetID() != room->GetID()) {
+                                   Log->Write(
+                                           "ERROR in PrepareForSimulation: ped %d want to leave roomID %d through doorUID %d (ID %d), but this room does not belong to this door.",
+                                           ped->GetID(), room->GetID(), doorUID, _CroTrByUID[doorUID]->GetID());
+                              }
+                         }
                     }
-                    subroomsDoorsVector.emplace_back(std::make_pair(subroom->GetUID(), doorUID));
+                    roomsDoorsVector.emplace_back(std::make_pair(room->GetID(), doorUID));
                     auto nextDoorUID = _pathsMatrix.at(std::make_pair(doorUID, finalDoor));
-                    // _pathsMatrix sometimes returns a door in the same subroom we are already in
-                    if (!_CroTrByUID[nextDoorUID]->IsInSubRoom(subroom->GetSubRoomID())) {
-                         auto oldSubroomID = subroom->GetSubRoomID();
-                         subroom = _CroTrByUID[doorUID]->GetOtherSubRoom(subroom->GetRoomID(), subroom->GetSubRoomID());
-                         if (!_CroTrByUID[nextDoorUID]->IsInSubRoom(subroom->GetSubRoomID())) {
-                              Log->Write("ERROR in PrepareForSimulation: ped %d wants to walk through doorUID %d (ID %d), but neither subroomID %d nor %d are adjacent.",
-                              ped->GetID(), nextDoorUID, _CroTrByUID[nextDoorUID]->GetID(), oldSubroomID, subroom->GetSubRoomID());
+                    // _pathsMatrix sometimes returns a door in the same room we are already in
+                    if (!_CroTrByUID[nextDoorUID]->IsInRoom(room->GetID())) {
+                         if (Transition* tr = dynamic_cast<Transition*>(_CroTrByUID[doorUID])) {
+                              auto oldRoomID = room->GetID();
+                              room = tr->GetOtherRoom(room->GetID());
+                              if (!tr->IsInRoom(room->GetID())) {
+                                   Log->Write("ERROR in PrepareForSimulation: ped %d wants to walk through doorUID %d (ID %d), but neither roomID %d nor %d are adjacent.",
+                                              ped->GetID(), nextDoorUID, _CroTrByUID[nextDoorUID]->GetID(), oldRoomID, room->GetID());
+                              }
                          }
                     }
                     doorUID = nextDoorUID;
                } while (doorUID != finalDoor);
-               Log->Write("PrepareForSimulation: ped %d needs ff in subroomUID %d (ID %d) to doorUID %d (ID %d) (final exit)",
-                          ped->GetID(), subroom->GetUID(), subroom->GetSubRoomID(), doorUID, _CroTrByUID[doorUID]->GetID());
-               if (_CroTrByUID[doorUID]->GetSubRoom1() &&
-                   _CroTrByUID[doorUID]->GetSubRoom1()->GetUID() != subroom->GetUID() &&
-                   _CroTrByUID[doorUID]->GetSubRoom2() &&
-                   _CroTrByUID[doorUID]->GetSubRoom2()->GetUID() != subroom->GetUID()) {
-                    Log->Write(
-                            "ERROR in PrepareForSimulation: ped %d want to leave subroomUID %d (ID %d) through doorUID %d (ID %d), but this subroom does not belong to this door.",
-                            ped->GetID(), subroom->GetUID(), subroom->GetSubRoomID(), doorUID, _CroTrByUID[doorUID]->GetID());
+               Log->Write("PrepareForSimulation: ped %d needs ff in roomID %d to doorUID %d (ID %d) (final exit)",
+                          ped->GetID(), room->GetID(), doorUID, _CroTrByUID[doorUID]->GetID());
+               if (_CroTrByUID[doorUID]->GetRoom1() &&
+                   _CroTrByUID[doorUID]->GetRoom1()->GetID() != room->GetID()) {
+                    if (auto tr = dynamic_cast<Transition*>(_CroTrByUID[doorUID])) {
+                         if (tr->GetRoom2() && tr->GetRoom2()->GetID() != room->GetID()) {
+                              Log->Write(
+                                      "ERROR in PrepareForSimulation: ped %d want to leave roomID %d through doorUID %d (ID %d) (final exit), but this room does not belong to this door.",
+                                      ped->GetID(), room->GetID(), doorUID, _CroTrByUID[doorUID]->GetID());
+                         }
+                    }
                }
-               subroomsDoorsVector.emplace_back(std::make_pair(subroom->GetUID(), doorUID));
+               roomsDoorsVector.emplace_back(std::make_pair(room->GetID(), doorUID));
           }
-          // make entries unique
+
+          // make entries unique // @todo f.mack maybe using a set is more efficient
+//#pragma omp single
+          std::sort(roomsDoorsVector.begin(), roomsDoorsVector.end());
+//#pragma omp single
+          roomsDoorsVector.erase(std::unique(roomsDoorsVector.begin(), roomsDoorsVector.end()), roomsDoorsVector.end());
+
+          for (auto sdIt : roomsDoorsVector) {
+               Log->Write("need ff in roomID %d for doorUID %d (ID %d)",
+                          sdIt.first, building->GetRoom(sdIt.first)->GetID(), sdIt.second, _CroTrByUID[sdIt.second]->GetID());
+          }
           // calculate the ff in a parallelized way
      }
 }
