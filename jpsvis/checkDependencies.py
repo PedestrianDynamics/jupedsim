63d275fdd766f0004d24eeae6ed739a6081a7bfd
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2016-11-12 02:28:56 +0100

    Clean up cmakefile

--- jpsvis/checkDependencies.py
+++ jpsvis/checkDependencies.py
@@ -3,24 +3,26 @@
 import glob2
 import subprocess
 import shlex
-import os 
+import os
+import sys
 RED = '\033[0;31m'
 NC = '\033[0m' # No Color
 
+if len(sys.argv) == 1:
+    sys.exit("usage: %s path_to_.app_file" % sys.argv[0])
+
+PATH = sys.argv[1] + "/Contents/Frameworks"
 
 CMD = "jpsvis"
-PATH = "./bin/%s.app/Contents/Frameworks"  % CMD
-
+# PATH = "./bin/%s.app/Contents/Frameworks"  % CMD
+# PATH = "/Volumes/jpsvis\ 0.7.0/jpsvis.app/Contents/Frameworks/"
 dependencies = glob2.glob("%s/**/*.dylib"%PATH)
-
-
-
 
 
 #VTK_LIBS
 for vtklib in dependencies:
     
-    print "vtklib: <%s>" % vtklib
+    print "lib: <%s>" % vtklib
 
     cmd = "otool -L %s" % vtklib
     f = open("blah.txt", "w")
@@ -34,11 +36,11 @@ for vtklib in dependencies:
            and not line.startswith("/System") \
            and not line.startswith("@executable_path"):
 
-            print ">> <%s>" % line.split()[0]
+            print "%s >> <%s> %s" % (RED, line.split()[0], NC)
             change = "install_name_tool -change %s @executable_path/../Frameworks/Python.framework/Versions/3.5/Python %s"%(line.split()[0], vtklib)
             print "<%s>" % change
             res = subprocess.call(shlex.split(change))
-
+            raw_input("pause ...")
             
     f.close()
 
