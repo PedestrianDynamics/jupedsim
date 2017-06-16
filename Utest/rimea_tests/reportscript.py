6dcfd93ed7fdb1fb70c0a9f44a63ae283a8f788d
Tao Zhong <taozhongimd@gmail.com>
2017-06-16 23:56:17 +0200

    reformat block comments

--- Utest/rimea_tests/reportscript.py
+++ Utest/rimea_tests/reportscript.py
@@ -1,18 +1,23 @@
 import os
 import re, sys, subprocess
 from pylatex import Document, Command, PageStyle, Head, MiniPage, LargeText, LineBreak, \
-	LineBreak, MediumText, LongTabu, NewPage, Package
+	LineBreak, MediumText, LongTabu, NewPage, Package, Center, Section
 from pylatex.utils import NoEscape, bold, verbatim
 from pylatex.base_classes import Environment
 
 
-## Import a number of test(int), return all evac time in the log of this test
-def get_evac_time(testnumber):
-	log_name = "./test_" + str(testnumber) + "/log_test_" + str(testnumber) + ".txt"
 
-	with open(log_name) as log:
+def get_evac_time(testnumber):
+		"""
+		Import a number of test
+		return all evac time in the log of this test
+		:param testnumber: int, the number of test
+		:return: list, contains all evacution times in a log txt
+		"""
+		log_name = "./test_" + str(testnumber) + "/log_test_" + str(testnumber) + ".txt"
+
 		evac_time = []
-		for line in log:
+		for line in log_name:
 			## Use Regular Expression to filter, fetch lines which contain the evac time.
 			if re.match(
 					r'^2017-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} - INFO - evac_time: \d{2}.\d{6} <= \d{2}.\d{6} <= \d{2}.\d{6}$',
@@ -22,56 +27,55 @@ def get_evac_time(testnumber):
 			else:
 				continue
 
-	return evac_time ## eventuelle nur last result
+		return evac_time ## even only last result
 
 
-## for the report within evacution time
 def generate_eva_report():
-	geometry_options = {
-		"head": "40pt",
-		"margin": "0.5in",
-		"bottom": "0.6in",
-		"includeheadfoot": True
-	}
-	doc = Document(geometry_options=geometry_options)
+		"""
+	  generate a report which contains the evacution time for every test
+	  """
+		geometry_options = {
+			"head": "40pt",
+			"margin": "0.5in",
+			"bottom": "0.6in",
+			"includeheadfoot": True
+		}
+		doc = Document(geometry_options=geometry_options)
 
-	## Define style of report
-	reportStyle = PageStyle("reportStyle")
+		## Define style of report
+		reportStyle = PageStyle("reportStyle")
 
-	with reportStyle.create(Head("R")) as left_header:
-		with left_header.create(MiniPage(width=NoEscape(r"0.49\textwidth"),
-		                                  pos='c', align='l')) as title_wrapper:
-			title_wrapper.append(LargeText(bold("RiMEA-Projekt")))
-			title_wrapper.append(LineBreak())
-			title_wrapper.append(MediumText(bold("Anlyse")))
+		with reportStyle.create(Head("R")) as left_header:
+			with left_header.create(MiniPage(width=NoEscape(r"0.49\textwidth"),
+			                                  pos='c', align='l')) as title_wrapper:
+				title_wrapper.append(LargeText(bold("RiMEA-Projekt")))
+				title_wrapper.append(LineBreak())
+				title_wrapper.append(MediumText(bold("Anlyse")))
 
-	doc.preamble.append(reportStyle)
+		doc.preamble.append(reportStyle)
 
-	## Apply Pagestyle
-	doc.change_document_style("reportStyle")
+		## Apply Pagestyle
+		doc.change_document_style("reportStyle")
 
-	## Create table
-	with doc.create(LongTabu("X[c] X[c]",
-	                         row_height=1.5)) as report_table:
-		report_table.add_row(["Test",
-		                    "evacuation time(s)"],
-		                   mapper=bold)
-		report_table.add_hline()
+		## Create table
+		with doc.create(LongTabu("X[c] X[c]",
+		                         row_height=1.5)) as report_table:
+			report_table.add_row(["Test",
+			                    "evacuation time(s)"],
+			                   mapper=bold)
+			report_table.add_hline()
 
-		for i in range(1,4):  
-			report_table.add_row(i, get_evac_time(i)[0])  
+			for i in range(1,4):
+				report_table.add_row(i, get_evac_time(i)[0])
 
-	doc.append(NewPage())
+		doc.append(NewPage())
 
-	doc.generate_pdf("RiMEA-Projekt Evacution Analyse", clean_tex=False)
+		doc.generate_pdf("RiMEA-Projekt Evacution Analyse", clean_tex=False)
 
 
 def generate_info_report():
 	"""
-	generate a report with funcs generate_cover for cover,
-	generate_status for tests status
-	generate_infos for last 2 lines of every log text
-	:return: null
+	generate a report with cover, status und last 2 lines info for every test
 	"""
 	geometry_options = {
 			"head": "40pt",
@@ -79,20 +83,22 @@ def generate_info_report():
 			"bottom": "0.6in",
 			"includeheadfoot": True
 		}
-	
+
 	doc = Document(documentclass='report', geometry_options=geometry_options)
 
 	## add preamble part
 	generate_cover(doc)
 
+
 	doc.generate_pdf("RiMEA-Projekt Analyse", clean_tex=False)
+
 
 
 def generate_cover(doc):
 	"""
 	generate a cover for generate_info_report func
 	cover contains name, date and branch info
-	:param doc: a Document Calss instance
+	:param doc: a Document Class instance
 	:return: null
 	"""
 
@@ -101,9 +107,7 @@ def generate_cover(doc):
 	doc.preamble.append(Command('date', get_git_status()[2]))
 
 	doc.packages.append(Package('titling'))
-
 	branch = r"\begin{center}Branch: "+ get_git_status()[0] + "\end{center}"
-
 	doc.preamble.append(Command('postdate', NoEscape(branch)))
 
 	doc.append(NoEscape(r'\maketitle'))
@@ -118,6 +122,8 @@ def get_git_status():
 	return branch, author, date
 
 if __name__ == "__main__":
-	generate_info_report()
+		generate_info_report()
 	# generate_eva_report()
+
+
 
