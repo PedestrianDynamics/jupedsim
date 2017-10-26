16cbcc433920b73f7b96e9370562ba7c271e8f91
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2017-10-26 16:06:26 +0200

    fix Plot_profiles

--- scripts/_Plot_profiles.py
+++ scripts/_Plot_profiles.py
@@ -7,17 +7,19 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import argparse
 import sys,glob
 
+# example usage:
+# python scripts/_Plot_profiles.py -p demos/corridor_JPScore/Output/Fundamental_Diagram/Classical_Voronoi/field -b 10 -n trajectory.xml -e 100 -x1 0 -x2 23 -y1 0 -y2 4
 
 def getParserArgs():
 	parser = argparse.ArgumentParser(description='Plot Profiles of density, velocity and flow')
 	parser.add_argument("-p", "--pathfile", default="./", help='give the path of source file')
-	parser.add_argument("-n", "--nametraj", help='give the name of the trajectory file')
-	parser.add_argument("-b", "--beginsteady", type=int, help='give the frame for the begining of steady state')
-	parser.add_argument("-e", "--endsteady", type=int, help='give the frame for the ending of steady state')
-	parser.add_argument("-x1", "--geominx", type=float, help='give the minmum x of the geometry')
-	parser.add_argument("-x2", "--geomaxx", type=float, help='give the maxmum x of the geometry')
-	parser.add_argument("-y1", "--geominy", type=float, help='give the minmum y of the geometry')
-	parser.add_argument("-y2", "--geomaxy", type=float, help='give the maxmum y of the geometry')
+	parser.add_argument("-n", "--nametraj", required=True, help='give the name of the trajectory file')
+	parser.add_argument("-b", "--beginsteady", type=int, required=True, help='give the frame for the beginning of steady state')
+	parser.add_argument("-e", "--endsteady", type=int, required=True, help='give the frame for the ending of steady state')
+	parser.add_argument("-x1", "--geominx", type=float, required=True, help='give the minmum x of the geometry')
+	parser.add_argument("-x2", "--geomaxx", type=float, required=True, help='give the maxmum x of the geometry')
+	parser.add_argument("-y1", "--geominy", type=float, required=True, help='give the minmum y of the geometry')
+	parser.add_argument("-y2", "--geomaxy", type=float, required=True, help='give the maxmum y of the geometry')
 	args = parser.parse_args()
 	return args
 
@@ -33,7 +35,13 @@ if __name__ == '__main__':
     geominY = args.geominy
     geomaxY = args.geomaxy
 
+    print(pathfile)
+    print(nametraj)
+    print(beginsteady)
+    print("Looking for: %s/density/*%s*%d.dat"%(pathfile,nametraj,beginsteady))
     f_Voronoi = glob.glob("%s/density/*%s*%d.dat"%(pathfile,nametraj,beginsteady))
+    if len(f_Voronoi) == 0:
+        sys.exit("no Voronoi files found in <%s/density/>" % pathfile)
     shape=shape(loadtxt(f_Voronoi[0]))
     density=zeros(shape)
     velocity=zeros(shape)
@@ -42,7 +50,9 @@ if __name__ == '__main__':
     ax1 = fig.add_subplot(111, aspect='1')
     plt.rc("font", size=40)
     for j in range(beginsteady,endsteady):
-        density+=loadtxt("%s/density/Prf_d_%s_"%(pathfile,nametraj)+str(j)+".dat")
+        density_file = "%s/density/Prf_d_%s_id_1_%.5d.dat"%(pathfile,nametraj, j)
+        print("loading: %s" % density_file)
+        density+=loadtxt(density_file)
     density=density/(endsteady-beginsteady)
     im = plt.imshow(density, cmap=cm.jet, interpolation='nearest',origin='lower',vmin=0,vmax=amax(density), extent=[geominX,geomaxX,geominY,geomaxY])
     ax1.set_xlabel("x [m]")
@@ -59,7 +69,10 @@ if __name__ == '__main__':
     ax1 = fig.add_subplot(111, aspect='1')
     plt.rc("font", size=40)
     for j in range(beginsteady,endsteady):
-        velocity+=loadtxt("%s/velocity/Prf_v_%s_"%(pathfile,nametraj)+str(j)+".dat")
+        velocity_file = "%s/velocity/Prf_v_%s_id_1_%.5d.dat"%(pathfile,nametraj, j)
+        print("loading: %s" % velocity_file)
+        velocity+=loadtxt(velocity_file)
+
     velocity=velocity/(endsteady-beginsteady)
     im = plt.imshow(velocity, cmap=cm.jet, interpolation='nearest',origin='lower',vmin=0,vmax=amax(velocity), extent=[geominX,geomaxX,geominY,geomaxY])
     ax1.set_xlabel("x [m]")
