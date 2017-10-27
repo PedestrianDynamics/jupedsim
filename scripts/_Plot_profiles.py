f6a1802e07d28537963f59adc14a5345ccfabcc2
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2017-10-27 11:09:39 +0200

    Untabify

--- scripts/_Plot_profiles.py
+++ scripts/_Plot_profiles.py
@@ -10,17 +10,17 @@ import os
 # python scripts/_Plot_profiles.py -p demos/corridor_JPScore/Output/Fundamental_Diagram/Classical_Voronoi/field -b 10 -n trajectory.xml -e 100 -x1 0 -x2 23 -y1 0 -y2 4
 
 def getParserArgs():
-	parser = argparse.ArgumentParser(description='Plot Profiles of density, velocity and flow')
-	parser.add_argument("-p", "--pathfile", default="./", help='give the path of source file')
-	parser.add_argument("-n", "--nametraj", required=True, help='give the name of the trajectory file')
-	parser.add_argument("-b", "--beginsteady", type=int, required=True, help='give the frame for the beginning of steady state')
-	parser.add_argument("-e", "--endsteady", type=int, required=True, help='give the frame for the ending of steady state')
-	parser.add_argument("-x1", "--geominx", type=float, required=True, help='give the minmum x of the geometry')
-	parser.add_argument("-x2", "--geomaxx", type=float, required=True, help='give the maxmum x of the geometry')
-	parser.add_argument("-y1", "--geominy", type=float, required=True, help='give the minmum y of the geometry')
-	parser.add_argument("-y2", "--geomaxy", type=float, required=True, help='give the maxmum y of the geometry')
-	args = parser.parse_args()
-	return args
+        parser = argparse.ArgumentParser(description='Plot Profiles of density, velocity and flow')
+        parser.add_argument("-p", "--pathfile", default="./", help='path to the field directory (some/path/field)')
+        parser.add_argument("-n", "--nametraj", required=True, help='name of the trajectory file')
+        parser.add_argument("-b", "--beginsteady", type=int, required=True, help='the frame for the beginning of steady state')
+        parser.add_argument("-e", "--endsteady", type=int, required=True, help='the frame for the ending of steady state')
+        parser.add_argument("-x1", "--geominx", type=float, required=True, help='the minmum x of the geometry')
+        parser.add_argument("-x2", "--geomaxx", type=float, required=True, help='the maxmum x of the geometry')
+        parser.add_argument("-y1", "--geominy", type=float, required=True, help='the minmum y of the geometry')
+        parser.add_argument("-y2", "--geomaxy", type=float, required=True, help='the maxmum y of the geometry')
+        args = parser.parse_args()
+        return args
 
 if __name__ == '__main__':
     args = getParserArgs()
@@ -37,9 +37,12 @@ if __name__ == '__main__':
     print("pathfile: %s" % pathfile)
     print("namtraj: %s" %nametraj)
     print("Steadystate: [%d, %d]" %(beginsteady, endsteady))
-    print("geometry: X [%.1f, %.1f], Y=[%.1f, %.1f]" %(geominX, geomaxX, geominY, geomaxY))
+    print("geometry: X [%.1f, %.1f], Y=[%.1f, %.1f]" %
+          (geominX, geomaxX, geominY, geomaxY))
 
-    density_files = os.path.join(pathfile, "density", "*%s*%d.dat" % (nametraj,beginsteady))
+    density_files = os.path.join(
+        pathfile, "density", "*%s*%d.dat" %
+        (nametraj,beginsteady))
     print("looking for %s" % density_files)
     f_Voronoi = glob.glob(density_files)
     # get the shape of the matrices
@@ -49,18 +52,23 @@ if __name__ == '__main__':
     shape = np.shape(np.loadtxt(f_Voronoi[0]))
     density = np.zeros(shape)
     velocity = np.zeros(shape)
-    #-------------------------------------density profile-----------------------------------------------------------------------
-    fig = plt.figure(figsize=(16*(geomaxX-geominX)/(geomaxY-geominY)+2, 16), dpi=100)
+    #-------------------------density profile----------------
+    fig = plt.figure(
+        figsize=(16*(geomaxX-geominX)/(geomaxY-geominY)+2, 16), dpi=100)
     ax1 = fig.add_subplot(111, aspect='1')
     plt.rc("font", size=40)
     for j in range(beginsteady,endsteady):
-        density_file = os.path.join(pathfile, "density", "Prf_d_%s_id_1_%.5d.dat" % (nametraj, j) )
+        density_file = os.path.join(
+            pathfile, "density", "Prf_d_%s_id_1_%.5d.dat" % (nametraj, j) )
         print("loading: %s" % density_file)
         density += np.loadtxt(density_file)
 
     density=density/(endsteady-beginsteady)
-    
-    im = plt.imshow(density, cmap=cm.jet, interpolation='nearest',origin='lower',vmin=0,vmax=np.amax(density), extent=[geominX,geomaxX,geominY,geomaxY])
+
+    im = plt.imshow(density, cmap=cm.jet,
+                    interpolation='nearest',origin='lower',
+                    vmin=0,vmax=np.amax(density),
+                    extent=[geominX,geomaxX,geominY,geomaxY])
     ax1.set_xlabel("x [m]")
     ax1.set_ylabel("y [m]")
     divider = make_axes_locatable(ax1)
@@ -71,17 +79,25 @@ if __name__ == '__main__':
     plt.savefig(density_figname)
     plt.close()
 
-    #---------------------------------velocity profile-------------------------------------------------------------
-    fig = plt.figure(figsize=(16*(geomaxX-geominX)/(geomaxY-geominY)+2, 16), dpi=100)
+    #-----------------velocity profile-----------------------------------
+    fig = plt.figure(
+        figsize=(16*(geomaxX-geominX)/(geomaxY-geominY)+2, 16),
+        dpi=100)
     ax1 = fig.add_subplot(111, aspect='1')
     plt.rc("font", size=40)
     for j in range(beginsteady,endsteady):
-        velocity_file = os.path.join(pathfile, "velocity", "Prf_v_%s_id_1_%.5d.dat" % (nametraj, j) )
+        velocity_file = os.path.join(
+            pathfile, "velocity", "Prf_v_%s_id_1_%.5d.dat" % (nametraj, j) )
         print("loading: %s" % velocity_file)
         velocity += np.loadtxt(velocity_file)
 
     velocity=velocity/(endsteady-beginsteady)
-    im = plt.imshow(velocity, cmap=cm.jet, interpolation='nearest',origin='lower',vmin=0,vmax=np.amax(velocity), extent=[geominX,geomaxX,geominY,geomaxY])
+    im = plt.imshow(velocity,
+                    cmap=cm.jet,
+                    interpolation='nearest',
+                    origin='lower',
+                    vmin=0,vmax=np.amax(velocity),
+                    extent=[geominX,geomaxX,geominY,geomaxY])
     ax1.set_xlabel("x [m]")
     ax1.set_ylabel("y [m]")
     divider = make_axes_locatable(ax1)
@@ -92,12 +108,16 @@ if __name__ == '__main__':
     plt.savefig(velocity_figname)
     plt.close()
 
-#---------------------------------flow profile-------------------------------------------------------------
-    fig = plt.figure(figsize=(16*(geomaxX-geominX)/(geomaxY-geominY)+2, 16), dpi=100)
+#-------------------flow profile-------------------------
+    fig = plt.figure(
+        figsize=(16*(geomaxX-geominX)/(geomaxY-geominY)+2, 16), dpi=100)
     ax1 = fig.add_subplot(111, aspect='1')
     plt.rc("font", size=40)
     flow=density*velocity
-    im = plt.imshow(flow, cmap=cm.jet, interpolation='nearest',origin='lower',vmin=0,vmax=np.amax(flow), extent=[geominX,geomaxX,geominY,geomaxY])
+    im = plt.imshow(flow,
+                    cmap=cm.jet, interpolation='nearest',origin='lower',
+                    vmin=0,vmax=np.amax(flow),
+                    extent=[geominX,geomaxX,geominY,geomaxY])
     ax1.set_xlabel("x [m]")
     ax1.set_ylabel("y [m]")
     divider = make_axes_locatable(ax1)
@@ -106,7 +126,7 @@ if __name__ == '__main__':
     cb.set_label('Specific flow [$1/m \cdot s$]')
     figname = "%s/profile_flux_%s.png"%(pathfile,nametraj)
     flux_figname = os.path.join(pathfile, "profile_flux_%s.png"% nametraj)
-    
+
     plt.savefig(flux_figname)
     plt.close()
 
@@ -114,5 +134,5 @@ if __name__ == '__main__':
     # print some infos
     print("\n\n============= \n---> %s" % density_figname)
     print("---> %s" % velocity_figname)
-    print("---> %s\n============= " % flux_figname)    
-    
+    print("---> %s\n============= " % flux_figname)
+
