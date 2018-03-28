17cf615bd1eac68bcd069d8bc0dea5f14569aaf4
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-03-28 16:02:08 +0200

    Intentation of file

--- methods/Method_A.cpp
+++ methods/Method_A.cpp
@@ -123,7 +123,7 @@ void Method_A::WriteFile_N_t(string data)
           string file_N_t ="Flow_NT_"+_trajName+"_id_"+_measureAreaId+".dat";
           if(_plotTimeSeries)
           {
-               string parameters_N_t="python3 \""+_scriptsLocation+"/_Plot_N_t.py\" -p \""+ METHOD_A_LOCATION + "\" -n "+file_N_t;
+               string parameters_N_t="python \""+_scriptsLocation+"/_Plot_N_t.py\" -p \""+ METHOD_A_LOCATION + "\" -n "+file_N_t;
                int res = system(parameters_N_t.c_str());
                Log->Write("INFO:\tPlotting N-t diagram! Status: %d", res);
           }
