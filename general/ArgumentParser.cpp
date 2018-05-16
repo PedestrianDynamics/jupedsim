8521543261706d8c830b1ecbe2143ef5388c839e
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-05-16 17:56:46 +0200

    :green_heart: add minGW to log message

--- general/ArgumentParser.cpp
+++ general/ArgumentParser.cpp
@@ -59,28 +59,32 @@ std::string ver_string(int a, int b, int c) {
       ss << a << '.' << b << '.' << c;
       return ss.str();
 }
-
+//https://sourceforge.net/p/predef/wiki/Compilers/
 std::string true_cxx =
 #ifdef __clang__
-   "clang++";
-#endif
-#ifdef __GNU__
-   "g++";
-#endif
-#ifdef _MSC_VER
-   "Visual Studio";
+      "clang++";
+#elif defined(__GNU__)
+"g++";
+#elif defined(__MINGW32__)
+   "MinGW";
+#elif defined(_MSC_VER)
+  "Visual Studio";
+#else
+"Compiler not identified";
 #endif
 
 
 std::string true_cxx_ver =
 #ifdef __clang__
     ver_string(__clang_major__, __clang_minor__, __clang_patchlevel__);
-#endif
-#ifdef __GNU__
+#elif defined(__GNU__)
     ver_string(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
-#endif
-#ifdef _MSC_VER
+#elif defined(__MINGW32__)
+ver_string(__MINGW32__, __MINGW32_MAJOR_VERSION, __MINGW32_MINOR_VERSION);
+#elif defined( _MSC_VER)
     ver_string(_MSC_VER, _MSC_FULL_VER,_MSC_BUILD);
+#else
+"";
 #endif
 
 
