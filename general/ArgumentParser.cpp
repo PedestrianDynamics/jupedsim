4f66c5feaf425af953fccaa5fa5e115a365a46f9
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-05-16 14:35:23 +0200

    fix for visualstudio detection

--- general/ArgumentParser.cpp
+++ general/ArgumentParser.cpp
@@ -60,24 +60,28 @@ std::string ver_string(int a, int b, int c) {
       return ss.str();
 }
 
-  std::string true_cxx =
+std::string true_cxx =
 #ifdef __clang__
    "clang++";
-#else
+#endif
+#ifdef __GNU__
    "g++";
 #endif
-
-  std::string true_cxx_ver =
-#ifdef __clang__
-    ver_string(__clang_major__, __clang_minor__, __clang_patchlevel__);
-#else
-    ver_string(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+#ifdef _MSC_VER
+   "Visual Studio";
 #endif
 
-// todo: handle Visual Studio
-/* #ifdef _MSC_VER */
-/*     std::to_string(_MSC_VER) */
-/* #endif */
+
+std::string true_cxx_ver =
+#ifdef __clang__
+    ver_string(__clang_major__, __clang_minor__, __clang_patchlevel__);
+#endif
+#ifdef __GNU__
+    ver_string(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__);
+#endif
+#ifdef _MSC_VER
+    ver_string(_MSC_VER, _MSC_FULL_VER,_MSC_BUILD);
+#endif
 
 
 
