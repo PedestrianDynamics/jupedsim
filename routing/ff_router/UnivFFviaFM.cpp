a4327e042d69fdab2e652bb3505f362b83cde0dc
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2019-06-11 14:57:42 +0200

    disable some warnings

--- routing/ff_router/UnivFFviaFM.cpp
+++ routing/ff_router/UnivFFviaFM.cpp
@@ -1817,7 +1817,7 @@ void UnivFFviaFM::getDirectionToUID(int destID, long int key, Point& direction){
         } else if ((key < _grid->GetnPoints()-_grid->GetiMax()) && (_gridCode[key+_grid->GetiMax()] != OUTSIDE) && (_gridCode[key+_grid->GetiMax()] != WALL)) {
             key = key + _grid->GetiMax();
         } else {
-            Log->Write("ERROR:\t In getDirectionToUID (3 args)");
+            // Log->Write("ERROR:\t In getDirectionToUID (3 args)");
         }
     }
      if (_directionFieldWithKey.count(destID)==1 && _directionFieldWithKey[destID]) {
