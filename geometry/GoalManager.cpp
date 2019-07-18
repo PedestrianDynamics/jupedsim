82e8390eceed7cd056bdc8daf4d313251fb26c3c
schroedtert <tobias.schroedter@rwth-aachen.de>
2019-07-18 15:56:27 +0200

    Added missing documentation

--- geometry/GoalManager.cpp
+++ geometry/GoalManager.cpp
@@ -1,6 +1,28 @@
-//
-// Created by Tobias Schrödter on 2019-02-17.
-//
+/**
+ * \file        GoalManager.cpp
+ * \date        Feb 17, 2019
+ * \version     v0.8
+ * \copyright   <2016-2022> Forschungszentrum Jülich GmbH. All rights reserved.
+ *
+ * \section License
+ * This file is part of JuPedSim.
+ *
+ * JuPedSim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * JuPedSim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with JuPedSim. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * \section Description
+ * Class mananging pedestrians who enter/leave waiting areas
+ */
 
 #include "GoalManager.h"
 
@@ -17,20 +39,30 @@ void GoalManager::ProcessPedPosition(Pedestrian* ped)
      // Ped is in current waiting area
      if (CheckInsideWaitingArea(ped, ped->GetFinalDestination())){
           WaitingArea* wa = dynamic_cast<WaitingArea*>(_allGoals[ped->GetFinalDestination()]);
+          // Add ped to waiting area
           wa->AddPed(ped->GetID());
+          // Ped enters waiting area
           ped->EnterGoal();
+
+          // Check state of waiting area
           if (!wa->IsOpen()) {
                SetState(wa->GetId(), false);
           }
      }
 
+     // Ped already had a goal AND has not reached final goal
      if ((ped->GetLastGoalID() != -1 ) && (ped->GetFinalDestination() != ped->GetLastGoalID())){
+          // Ped is not in waiting area which was the last goal
           if (!CheckInsideWaitingArea(ped, ped->GetLastGoalID())){
                WaitingArea* wa = dynamic_cast<WaitingArea*>(_allGoals[ped->GetLastGoalID()]);
 
                if (wa != nullptr){
+                    // Remove ped from waiting area
                     wa->RemovePed(ped->GetID());
+                    // ped leaves waiting area
                     ped->LeaveGoal();
+
+                    // Check state of waiting area
                     if (wa->IsOpen()) {
                          SetState(wa->GetId(), true);
                     }
