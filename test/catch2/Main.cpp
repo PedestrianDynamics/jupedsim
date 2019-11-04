a4497787bf4d4d4b19be17477f04e012b814f47f
David Haensel <d.haensel@fz-juelich.de>
2019-11-04 11:42:13 +0100

    Fixes Logging output in unittest crashing CI

--- test/catch2/Main.cpp
+++ test/catch2/Main.cpp
@@ -1,2 +1,16 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
+#include "general/Logger.h"
+
 #include <catch2/catch.hpp>
+
+int main(int argc, char * argv[])
+{
+    Logging::Guard guard;
+    Logging::SetLogLevel(Logging::Level::Off);
+
+    int result = Catch::Session().run(argc, argv);
+
+    // global clean-up...
+
+    return result;
+}
