fe320a3ddc4ac25854415b8f6a73774da96aff4e
David Haensel <d.haensel@fz-juelich.de>
2019-11-22 17:53:28 +0100

    Fixes binary file path. This is just an intermediate fix.
    We need a testrunner accepting the bin path as parameter

--- systemtest/JPSRunTest.py
+++ systemtest/JPSRunTest.py
@@ -70,7 +70,7 @@ class JPSRunTestDriver(object):
     def run_test(self, testfunction, fd=0, *args): #fd==1: make fundamental diagram
         assert hasattr(testfunction, '__call__'), "run_test: testfunction has no __call__ function"
         self.__configure()
-        jpscore = os.path.join(self.trunk, "bin", "jpscore")
+        jpscore = os.path.join(self.trunk, "build", "bin", "jpscore")
         jpscore_exe = self.__find_executable(jpscore)
         results = []
         for inifile in self.inifiles:
