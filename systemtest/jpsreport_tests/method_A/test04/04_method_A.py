eaf22278c7b3e84bd3959601bebffee654bda440
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2020-02-20 19:18:20 +0100

    Refactoring testing of jpsreport (#668)
    Refactoring testing of jpsreport
    
    Tests for jpsreport are included in the testing machinery as it is the case for jpscore.
    
    This is important so that future changes in jpsreport can be testes by CI.

--- systemtest/jpsreport_tests/method_A/test04/04_method_A.py
+++ systemtest/jpsreport_tests/method_A/test04/04_method_A.py
@@ -12,12 +12,13 @@ path.append(utestdir)
 path.append(os.path.dirname(path[0])) # source helper file
 from utils import SUCCESS, FAILURE
 import numpy as np
-import subprocess
+from JPSRunTest import JPSRunTestDriver
+
 # adding 9 frames (fps=8). The distance for the first agent takes exactly 7 seconds.
 should_be_7 = np.array([7 + 1.125, 11 + 1.125, 16 + 1.125, 21 + 1.125, 31 + 1.125, 36 + 1.125])
 should_be_9 = should_be_7 + 2
 
-def runtest(trajfile):
+def runtest(inifile, trajfile):
     logging.info("===== Method A - Flow-NT ===============")
     data_9_filename = os.path.join('./Output',
                                    'Fundamental_Diagram',
@@ -26,14 +27,16 @@ def runtest(trajfile):
                                    )
     
     if not os.path.exists(data_9_filename):
-        logging.error("jpsreport did not output results correctly.")
+        logging.critical("jpsreport did not output results correctly.")
         exit(FAILURE)
+
     data_9 = np.loadtxt(data_9_filename)
     time_change_9 = data_9[np.nonzero(np.diff(data_9[:, 1]) >0)][:, 0]
     if np.all(np.abs(time_change_9 - should_be_9) < 0.5):
-        print("Got", time_change_9, ", expected", should_be_9)
+        logging.info("should_be_9 OK.")
     else:
-        print("Got", time_change_9, ", expected", should_be_9)
+        logging.critical("should_be_9 did not match result. Got {}. Expected {}".format(time_change_9, should_be_9))
+        exit(FAILURE)      
 
     data_7_filename = os.path.join('./Output',
                                    'Fundamental_Diagram',
@@ -41,25 +44,21 @@ def runtest(trajfile):
                                    'Flow_NT_%s_id_7.dat'%trajfile
                                    )
     if not os.path.exists(data_7_filename):
-        logging.error("jpsreport did not output results correctly.")
+        logging.critical("jpsreport did not output results correctly.")
         exit(FAILURE)
 
     data_7 = np.loadtxt(data_7_filename)
     time_change_7 = data_7[np.nonzero(np.diff(data_7[:, 1]) >0)][:, 0]
 
     if np.all(np.abs(time_change_7 - should_be_7) < 0.5):
-        print("Got", time_change_7, ", expected", should_be_7)
+        logging.info("should_be_7 OK.")
     else:
-        print("Error. Got", time_change_7, ", expected", should_be_7)
+        logging.critical("should_be_7 did not match result. Got {}. Expected {}".format(time_change_7, should_be_7))
         exit(FAILURE)
 
 
 if __name__ == "__main__":
-    if len(argv) < 4:
-        exit("usage: %s jpsreport_path inifile trajfile"%argv[0])
-    jpsreport = argv[1]
-    inifile = argv[2]
-    trajfile = argv[3]
-    subprocess.call([jpsreport, "%s" % inifile])
-    runtest(trajfile)
+    test = JPSRunTestDriver(4, argv0=argv[0], testdir=path[0], utestdir=utestdir, jpsreport=argv[1])
+    test.run_analysis(trajfile="traj.txt", testfunction=runtest)
+    logging.info("%s exits with SUCCESS" % (argv[0]))
     exit(SUCCESS)
