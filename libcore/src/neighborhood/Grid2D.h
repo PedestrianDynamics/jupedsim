26b22646417c4538cb6725e80ece1961e831e46c
David Haensel <d.haensel@fz-juelich.de>
2020-04-02 12:03:31 +0200

    Grid2D unittests

--- libcore/src/neighborhood/Grid2D.h
+++ libcore/src/neighborhood/Grid2D.h
@@ -32,10 +32,10 @@ private:
     using container_type = std::vector<element_type>;
 
 public:
-    using size_type  = std::size_t;
-    using value_type = T;
-    using row_type   = container_type<value_type>;
-    using grid_type  = container_type<row_type>;
+    using size_type          = std::size_t;
+    using value_type         = T;
+    using row_type           = container_type<value_type>;
+    using row_container_type = container_type<row_type>;
 
     Grid2D() = default;
 
@@ -45,9 +45,9 @@ public:
     Grid2D & operator=(const Grid2D &) = default;
     Grid2D & operator=(Grid2D &&) = default;
 
-    Grid2D(size_type num_rows, size_type num_columns) : _grid(num_rows, row_type(num_columns)) {}
+    Grid2D(size_type num_rows, size_type num_columns) : _rows(num_rows, row_type(num_columns)) {}
 
-    row_type const & operator[](size_type row) const { return _grid[row]; }
+    row_type const & operator[](size_type row) const { return _rows[row]; }
     row_type & operator[](size_type row)
     {
         return const_cast<row_type &>(const_cast<const Grid2D *>(this)->operator[](row));
@@ -55,13 +55,24 @@ public:
 
     void clear()
     {
-        for(auto & row : _grid) {
+        for(auto & row : _rows) {
             for(auto & elem : row) {
                 elem.clear();
             }
         }
     }
 
+    size_type num_rows() const { return _rows.size(); }
+    size_type num_columns() const
+    {
+        if(_rows.empty()) {
+            return 0;
+        } else {
+            return _rows.front().size();
+        }
+    }
+
+
 private:
-    grid_type _grid;
+    row_container_type _rows;
 };
--- libcore/test/catch2/neighborhood/Grid2D.cpp
+++ libcore/test/catch2/neighborhood/Grid2D.cpp
@@ -24,14 +24,105 @@
 #include <catch2/catch.hpp>
 #include <deque>
 
-TEST_CASE("neighborhood/Grid2D", "[neighborhood][neightbor-search][grid]")
+TEST_CASE("neighborhood/Grid2D", "[neighborhood][neightbor-search][grid2d]")
 {
-    SECTION("Grid2D")
+    SECTION("ctors")
     {
-        Grid2D<std::deque<Pedestrian *>> grid(10, 100);
+        SECTION("empty default")
+        {
+            Grid2D<int> grid;
+            REQUIRE(grid.num_rows() == 0);
+            REQUIRE(grid.num_columns() == 0);
 
-        for(unsigned i = 0; i < 10; ++i) {
-            REQUIRE(grid[i].size() == 100);
+            Grid2D<std::deque<Pedestrian *>> grid2;
+            REQUIRE(grid2.num_rows() == 0);
+            REQUIRE(grid2.num_columns() == 0);
+        }
+
+        SECTION("custom contructor")
+        {
+            Grid2D<std::deque<Pedestrian *>> grid(10, 100);
+            REQUIRE(grid.num_rows() == 10);
+            REQUIRE(grid.num_columns() == 100);
+
+            for(unsigned i = 0; i < 10; ++i) {
+                REQUIRE(grid[i].size() == 100);
+            }
+
+            Grid2D<std::deque<int>> grid2(7, 9);
+            REQUIRE(grid2.num_rows() == 7);
+            REQUIRE(grid2.num_columns() == 9);
+
+            for(unsigned i = 0; i < 7; ++i) {
+                REQUIRE(grid2[i].size() == 9);
+            }
+        }
+
+        SECTION("copy")
+        {
+            SECTION("empty values")
+            {
+                Grid2D<std::deque<int>> grid(5, 10);
+                REQUIRE(grid.num_rows() == 5);
+                REQUIRE(grid.num_columns() == 10);
+                for(unsigned i = 0; i < 5; ++i) {
+                    for(unsigned j = 0; j < 10; ++j) {
+                        // the deque should be empty
+                        REQUIRE(grid[i][j].empty());
+                    }
+                }
+
+                auto grid_copy = grid;
+                // grid must be the same, it is a copy, not move
+                REQUIRE(grid.num_rows() == 5);
+                REQUIRE(grid.num_columns() == 10);
+                for(unsigned i = 0; i < 5; ++i) {
+                    for(unsigned j = 0; j < 10; ++j) {
+                        // the deque should be empty
+                        REQUIRE(grid[i][j].empty());
+                    }
+                }
+
+                // now check the copy
+                REQUIRE(grid_copy.num_rows() == 5);
+                REQUIRE(grid_copy.num_columns() == 10);
+                for(unsigned i = 0; i < 5; ++i) {
+                    for(unsigned j = 0; j < 10; ++j) {
+                        // the deque should be empty
+                        REQUIRE(grid_copy[i][j].empty());
+                    }
+                }
+            }
+            SECTION("non empty")
+            {
+                Grid2D<int> grid(5, 10);
+                REQUIRE(grid.num_rows() == 5);
+                REQUIRE(grid.num_columns() == 10);
+                for(unsigned i = 0; i < 5; ++i) {
+                    for(unsigned j = 0; j < 10; ++j) {
+                        grid[i][j] = i * j;
+                    }
+                }
+
+                auto grid_copy = grid;
+                // grid must be the same, it is a copy, not move
+                REQUIRE(grid.num_rows() == 5);
+                REQUIRE(grid.num_columns() == 10);
+                for(unsigned i = 0; i < 5; ++i) {
+                    for(unsigned j = 0; j < 10; ++j) {
+                        REQUIRE(grid[i][j] == i * j);
+                    }
+                }
+
+                // now check the copy
+                REQUIRE(grid_copy.num_rows() == 5);
+                REQUIRE(grid_copy.num_columns() == 10);
+                for(unsigned i = 0; i < 5; ++i) {
+                    for(unsigned j = 0; j < 10; ++j) {
+                        REQUIRE(grid_copy[i][j] == i * j);
+                    }
+                }
+            }
         }
     }
 }
