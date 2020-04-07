946a9755b1717e25ff04682d2ddb9e017f4b2288
David Haensel <d.haensel@fz-juelich.de>
2020-04-07 12:05:55 +0200

    Copyright notice and License header reworked.
     - Separates Doxygen comments and description from License related
    stuff.
     - Removes Doxygen commands from Copyright Notice and License notice.
     - Removes date from doxygen comment

--- libcore/src/Simulation.h
+++ libcore/src/Simulation.h
@@ -1,24 +1,26 @@
+/** JuPedSim -- Jülich Pedestrian Dynamics Simulator
+ *
+ *  Copyright <2009-2020> Forschungszentrum Jülich GmbH. All rights reserved.
+ *
+ *  This file is part of JuPedSim.
+ *
+ *  JuPedSim is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  JuPedSim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with JuPedSim.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /**
- * \file        Simulation.h
- * \date        Dec 15, 2010
- * \version     v0.7
- * \copyright   <2009-2015> Forschungszentrum J?lich GmbH. All rights reserved.
- *
- * \section License
- * This file is part of JuPedSim.
- *
- * JuPedSim is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * JuPedSim is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with JuPedSim. If not, see <http://www.gnu.org/licenses/>.
+ * \file    Simulation.h
+ * \version v0.7
  *
  * \section Description
  * The Simulation class represents a simulation of pedestrians
@@ -26,7 +28,8 @@
  * various parameters and functions.
  *
  *
- **/
+ */
+
 #pragma once
 
 #include "IO/OutputHandler.h"
