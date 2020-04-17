304ce8b12701edb02bc317a8ff403c7ee9767fcb
schroedtert <tobias.schroedter@rwth-aachen.de>
2020-04-17 10:00:28 +0200

    Also update router when train arrives/departs

--- libcore/src/Simulation.cpp
+++ libcore/src/Simulation.cpp
@@ -427,7 +427,7 @@ double Simulation::RunBody(double maxSimTime)
             }
 
             // here the used routers are update, when needed due to external changes
-            if(_routingEngine->NeedsUpdate()) {
+            if(_routingEngine->NeedsUpdate() || geometryChanged) {
                 LOG_INFO("Update router during simulation.");
                 _routingEngine->UpdateRouter();
             }
