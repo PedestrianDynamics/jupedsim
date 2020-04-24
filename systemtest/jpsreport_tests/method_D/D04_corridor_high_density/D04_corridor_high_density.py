cb23998816c5101146fbfacc9e2ba9ec9471c31e
Jette Schumann <j.schumann@fz-juelich.de>
2020-04-24 17:03:04 +0200

    Created tests for validating the calculation of the classical Voronoi data with method D. Tests are calculating with artificial trajectories which are a grid that is moving with constant speed through a corridor. Python script for creating them is provided. Test for low/medium/high density available based on the grid properties (vertical and horizontal distance).
    Decided to use Method name and number as prefix for test names.

---
+++ systemtest/jpsreport_tests/method_D/D04_corridor_high_density/D04_corridor_high_density.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# ---------
+# Test of classical voronoi output
+# Pedestrians are moving in a 6x6 grid in x-direction with v=1 m/s
+# They have a fixed horizontal and vertical distance of 0.5m
+# Due to the alignment the inner voronoi cells should be squares
+# The inner voronoi cells should lie entirely in the specified square measurement area for frame 109
+# Real voronoi density is 1 person / (0.5m*0.5m)
+# Real velocity is 1.0 m/s for all frames (since cut off is false parts of the outer voronoi cells lie in the measurement area)
+# ---------
+import os
+from sys import argv, path
+import logging
+
+utestdir = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(path[0]))))
+path.append(utestdir)
+path.append(os.path.dirname(path[0]))  # source helper file
+from utils import SUCCESS, FAILURE
+import numpy as np
+from JPSRunTest import JPSRunTestDriver
+
+# real density can be calculated based on the square
+real_density = 1 / (0.5 ** 2)
+# real velocity must remain the same
+real_velocity = 1
+# accepted error
+acceptance_range = 0.001
+# frame when inner vornoi cells of 16 people are entirely in the measurement area
+frame = 109
+
+
+def runtest(inifile, trajfile):
+    logging.info("===== Method D - Classical Voronoi ===============")
+    jpsreport_result_file = os.path.join('./Output',
+                                         'Fundamental_Diagram',
+                                         'Classical_Voronoi',
+                                         'rho_v_Voronoi_%s_id_1.dat' % trajfile
+                                         )
+
+    if not os.path.exists(jpsreport_result_file):
+        logging.critical("jpsreport did not output results correctly.")
+        exit(FAILURE)
+
+    jpsreport_data = np.loadtxt(jpsreport_result_file)
+
+    # check density for one frame
+    jpsreport_density = jpsreport_data[jpsreport_data[:, 0] == frame][0, 1]
+
+    if abs(jpsreport_density - real_density) < acceptance_range:
+        logging.info("density in measurement area should be OK.")
+    else:
+        logging.critical(
+            "density value in measurement area did not match result. Got {}. Expected {}".format(jpsreport_density,
+                                                                                                 real_density))
+        exit(FAILURE)
+
+    # check velocity for all frames
+    jpsreport_velocity = jpsreport_data[:, 2]
+    real_velocity_array = np.ones(np.size(jpsreport_velocity)) * real_velocity
+
+    if np.all(np.abs(jpsreport_velocity - real_velocity_array) < acceptance_range):
+        logging.info("velocity calculation should be OK.")
+    else:
+        logging.critical(
+            "velocity values did not match result. Got {}. Expected {}".format(jpsreport_velocity, real_velocity_array))
+        exit(FAILURE)
+
+
+if __name__ == "__main__":
+    test = JPSRunTestDriver(4, argv0=argv[0], testdir=path[0], utestdir=utestdir, jpsreport=argv[1])
+    test.run_analysis(trajfile="trajectory_grid_6x6.txt", testfunction=runtest)
+    logging.info("%s exits with SUCCESS" % (argv[0]))
+    exit(SUCCESS)
---
+++ systemtest/jpsreport_tests/method_D/D04_corridor_high_density/create_trajectories.py
@@ -0,0 +1,51 @@
+# Script for creating artifical trajectories for testing purposes.
+# Pedestrians move with a constant speed und do not interact with each other.
+# They are aligned in a grid that is moved along x-axis for each frame.
+
+startPosX = 0.0  # start position, upper left corner of the grid
+startPosY = 3.0
+numPedsX = 6  # number of columns of pedestrians in the grid
+numPedsY = 6  # number of rows of pedestrians in the grid
+dist = 0.5  # distance between the pedestrians (vertically and horizontally)
+v = 1  # x-velocity in m/s
+sim_time = 20  # simulation time in s
+fps = 12  # frames per second
+file_name = "trajectory_grid_" + str(numPedsX) + "x" + str(numPedsY) + ".txt"
+geometry_file = "geometry.xml"
+
+# define fixed ellipse parameters to allow visualization with jpsvis
+a = 0.2
+b = 0.2
+angle = 0.0
+color = 220
+
+sim_frames = sim_time * fps
+
+f = open(file_name, "w")
+# write header
+
+f.write("#framerate: {:.2f}\n#geometry: {}".format(fps, geometry_file))
+
+f.write(
+    "#ID: the agent ID \n#FR: the current frame\n#X,Y,Z: the agents coordinates (in metres)\n#A, B: semi-axes of the ellipse\n#ANGLE: orientation of the ellipse\n#COLOR: color of the ellipse\n\n#ID\tFR\tX\tY\tZ\tA\tB\tANGLE\tCOLOR\n")
+
+for frame in range(sim_frames):
+
+    # calc position of peds in grid based on start pos
+    for i in range(numPedsX):
+        for j in range(numPedsY):
+            # calc id based on current row and column
+            id = numPedsX * j + i + 1
+
+            xPos = startPosX + i * dist
+            yPos = startPosY - j * dist
+
+            f.write(
+                "{:6d}\t{:6d}\t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\t{:.6f}\t{:d}\n".format(id, frame + 1, xPos, yPos,
+                                                                                              0.0, a, b,
+                                                                                              angle, color))
+
+    # move grid to the right for next frame
+    startPosX += v / fps
+
+f.close()
