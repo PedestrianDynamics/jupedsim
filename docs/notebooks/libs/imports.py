6bca1499c7529fa91abe6150c3b1526751bdc6e9
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2020-05-12 09:56:27 +0200

    Cleanup imports
    Remove unneeded imports

--- docs/notebooks/libs/imports.py
+++ docs/notebooks/libs/imports.py
@@ -1,38 +1,31 @@
-from __future__ import print_function
 import os
-import re
-import glob
+import re # str_to_array help function 
+import glob # glob over result files
 
 import numpy as np
 import pandas as pd
 
+#---------------- for ploting and animating ------------------------
 import matplotlib.pyplot as plt
-
 import matplotlib.cm as cm
 import matplotlib.animation as animation
-from matplotlib import rc
-from matplotlib import colors
-
-from matplotlib.patches import Polygon as ppolygon
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 from matplotlib.animation import FuncAnimation
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes #colorbar
+from matplotlib.patches import Polygon as ppolygon # polygons
+from mpl_toolkits.axes_grid1 import make_axes_locatable # profiles
+#-----------------------------------------------------------------
 
-from xml.dom.minidom import parse
-
-import mpl_toolkits.axes_grid1
-import matplotlib.widgets
-
+# Visualisation
 from IPython.display import HTML
 
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
-from ipywidgets import IntSlider
-
-from utils import * 
-
+# parsing xml files
 from xml.dom.minidom import parse
+
+# Widgets 
+import ipywidgets as widgets
+
+# a colection of helper functions 
+from utils import * 
 
 
 
