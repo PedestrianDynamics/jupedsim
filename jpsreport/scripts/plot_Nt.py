8a42c1ede6d632b2942e241c3492eb0c6622ae99
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2020-05-25 23:39:26 +0200

    Refactoring plot Nt

--- jpsreport/scripts/plot_Nt.py
+++ jpsreport/scripts/plot_Nt.py
@@ -1,12 +1,17 @@
-# run script without arguments for help
+# Usage: 
+# plot_Nt.py --help
+#------------------
 import sys
 import os
 from xml.dom.minidom import parse
 import glob
 
-from utilities import get_parser_args_Nt
-from utilities import plot_nt
-from utilities import get_ids_nt_files
+if "helpers" not in sys.path:
+    sys.path.append("helpers")
+
+from Parser import get_parser_args_Nt
+from Ploter import plot_nt
+from Utilities import get_ids_nt_files
 
     
 if __name__ == '__main__':
@@ -17,13 +22,15 @@ if __name__ == '__main__':
         sys.exit("{} does not exist".format(jpsreport_inifile))
         
     d = parse(jpsreport_inifile)
-    output_dir =  d.getElementsByTagName('output')[0].attributes.items()[0][1]
-    jpsreport_ini_dir = os.path.dirname(jpsreport_inifile)
-    flow_nt_files = os.path.join(jpsreport_ini_dir, output_dir, "Fundamental_Diagram", "FlowVelocity", "Flow_NT_*.dat")
+    # is method A being used?
     method_A =  d.getElementsByTagName('method_A')[0].attributes.items()[0][1]
     if method_A.lower() == "false":
         sys.exit("method A ist false. Nothing to plot") 
 
+    # get Nt-files
+    output_dir =  d.getElementsByTagName('output')[0].attributes.items()[0][1]
+    jpsreport_ini_dir = os.path.dirname(jpsreport_inifile)
+    flow_nt_files = os.path.join(jpsreport_ini_dir, output_dir, "Fundamental_Diagram", "FlowVelocity", "Flow_NT_*.dat")
     nt_files = glob.glob(flow_nt_files)
     if not nt_files:
         sys.exit("No Flow_NT files found in <{}>".format(flow_nt_files))
