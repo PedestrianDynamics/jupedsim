9d79f127557e7fc2b26095874f4dc456a2a6966f
schroedtert <tobias.schroedter@rwth-aachen.de>
2021-10-15 09:26:00 +0200

    Added Cpack for macos

--- jpsvis/cmake/install_helper.cmake
+++ jpsvis/cmake/install_helper.cmake
@@ -1,22 +1,135 @@
 #########################################################
 #                    INSTALL                            #
 #########################################################
-function (cpack_write_deb_config)
-    message(STATUS "Package generation - LINUX")
-    message(STATUS "not yet implemented")
-endfunction()
+macro(create_package)
+    write_package_information()
+    write_package_content()
+endmacro()
 
-function (cpack_write_osx_config)
-    message(STATUS "Package generation - MacOS")
-    message(STATUS "not yet implemented")
-endfunction()
+macro(write_package_information)
+    set(CPACK_PACKAGE_FILE_NAME "jpsvis-installer-${PROJECT_VERSION}")
+    set(CPACK_PACKAGE_VENDOR "Forschungszentrum Juelich GmbH")
+    set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERISON_MAJOR})
+    set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERISON_MINOR})
+    set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERISON_PATCH})
+    set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_SOURCE_DIR}/README.md")
+    set(CPACK_PACKAGE_HOMEPAGE_URL "http://jupedsim.org")
+    set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/README.md")
+    set(CPACK_PACKAGE_EXECUTABLES "jpsvis")
+    set(CPACK_MONOLITHIC_INSTALL TRUE)
+    set(CPACK_CREATE_DESKTOP_LINKS jpsvis)
+    set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE")
+    SET(CPACK_PACKAGE_DESCRIPTION "Visualisation module for JuPedSim")
+    SET(CPACK_PACKAGE_CONTACT "m.chraibi@fz-juelich.de")
 
-function (cpack_write_windows_config)
+    if (APPLE)
+        write_package_information_macos()
+    elseif(WIN32)
+        write_package_information_win()
+    elseif(UNIX)
+        write_package_information_unix()
+    endif()
+endmacro()
+
+macro(write_package_content)
+    include(InstallRequiredSystemLibraries)
+    include(GNUInstallDirs)
+
+    install(TARGETS jpsvis
+        BUNDLE DESTINATION .
+    )
+
+    install(
+        DIRECTORY ${CMAKE_SOURCE_DIR}/samples
+        DESTINATION .
+    )
+
+    if (APPLE)
+        write_package_content_macos()
+    elseif(WIN32)
+        write_package_content_win()
+    elseif(UNIX)
+        write_package_content_unix()
+    endif()
+
+    include(CPack)
+endmacro()
+
+################################################################################
+# MacOS Bundle
+################################################################################
+macro(write_package_information_macos)
+    set(CPACK_GENERATOR "DragNDrop")
+
+    SET(MACOSX_BUNDLE_ICON_FILE JPSvis.icns)
+    SET(MACOSX_BUNDLE_COPYRIGHT "Copyright (c) 2015-2021 Forschungszentrum Juelich. All rights reserved.")
+    SET(MACOSX_BUNDLE_INFO_STRING "Visualisation module for  JuPedSim, a framework for simulation and analysis of pedestrian dynamics.")
+    SET(MACOSX_BUNDLE_BUNDLE_NAME "JPSvis")
+    SET(MACOSX_BUNDLE_BUNDLE_VERSION "${PROJECT_VERSION}")
+    SET(MACOSX_BUNDLE_LONG_VERSION_STRING "version ${PROJECT_VERSION}")
+    SET(MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION}")
+    SET(MACOSX_GUI_COPYRIGHT "Copyright (c) 2015-2021 Forschungszentrum Juelich. All rights reserved.")
+    SET(MACOSX_BUNDLE_GUI_IDENTIFIER "www.jupedsim.org")
+endmacro()
+
+macro(write_package_content_macos)
+    set_target_properties(jpsvis PROPERTIES
+        MACOSX_BUNDLE_BUNDLE_VERSION       "${PROJECT_VERSION}"
+        MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION}"
+    )
+
+    set_target_properties(jpsvis PROPERTIES
+        INSTALL_RPATH @executable_path/../Frameworks
+    )
+
+    get_target_property(mocExe Qt5::moc IMPORTED_LOCATION)
+    get_filename_component(qtBinDir "${mocExe}" DIRECTORY)
+
+    find_program(DEPLOYQT_EXECUTABLE macdeployqt PATHS "${qtBinDir}" NO_DEFAULT_PATH)
+    set(DEPLOY_OPTIONS [[${CMAKE_INSTALL_PREFIX}/jpsvis.app -verbose=2 ]])
+    configure_file(${CMAKE_SOURCE_DIR}/cmake/deployapp.cmake.in deployapp.cmake @ONLY)
+    install(SCRIPT ${CMAKE_CURRENT_BINARY_DIR}/deployapp.cmake)
+
+    install(CODE [[
+      include(BundleUtilities)
+
+      # This string is crazy-long enough that it's worth folding into a var...
+      set (_plugin_file "$<TARGET_FILE_NAME:Qt5::QCocoaIntegrationPlugin>")
+
+      # Ditto the output paths for our installation
+      set (_appdir "${CMAKE_INSTALL_PREFIX}/jpsvis.app")
+      set (_outdir "${_appdir}/Contents/PlugIns/platforms")
+
+      file(INSTALL DESTINATION "${_outdir}"
+        TYPE FILE FILES "$<TARGET_FILE:Qt5::QCocoaIntegrationPlugin>")
+
+      fixup_bundle("${_appdir}/Contents/MacOS/jpsvis" "${_outdir}/${_plugin_file}" "")
+    ]] COMPONENT Runtime)
+
+endmacro()
+
+################################################################################
+# Windows Installer
+################################################################################
+macro(write_package_information_win)
     message(STATUS "Package generation - Windows")
     message(STATUS "not yet implemented")
-endfunction()
+endmacro()
 
-function (cpack_write_config)
-    message(STATUS "Cpack write configs")
+macro(write_package_content_win)
+    message(STATUS "Package generation - Windows")
     message(STATUS "not yet implemented")
-endfunction()
+endmacro()
+
+################################################################################
+# Deb package
+################################################################################
+macro(write_package_information_unix)
+    message(STATUS "Package generation - Deb")
+    message(STATUS "not yet implemented")
+endmacro()
+
+macro(write_package_content_unix)
+    message(STATUS "Package generation - Deb")
+    message(STATUS "not yet implemented")
+endmacro()
