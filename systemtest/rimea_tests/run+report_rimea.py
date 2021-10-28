06a8d8172f039e7958a9c7e6891a5a3da34ea513
Ozaq <ozaq@posteo.de>
2021-10-28 13:24:32 +0200

    Remove rimea tests

--- systemtest/rimea_tests/run+report_rimea.py
+++
@@ -1,9 +0,0 @@
-import runtest_execute
-import reportscript
-
-if __name__ == '__main__':
-#    runtest_execute.test()
-#    reportscript.test()
-    runtest_execute.runtest_execute(delete_logs=True)
-    reportscript.generate_info_report()
-    print('\nreportscript successfully created\n')
