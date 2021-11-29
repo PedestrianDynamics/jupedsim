f3ce4017ae1220f870b238b3a2ab062b7f1ad206
Jette Schumann <j.schumann@fz-juelich.de>
2021-11-29 16:03:08 +0100

    Result of pair programming

--- systemtest/driver/jpscore_test.py
+++ systemtest/driver/jpscore_test.py
@@ -7,76 +7,59 @@ import subprocess
 import pytest
 
 
-class JpsCoreDriver:
+class Environment:
     def __init__(self):
         self.jpscore_path = pathlib.Path(str(os.getenv("JPSCORE_EXECUTABLE_PATH")))
-        self.system_test_dir = pathlib.Path().absolute().parent
+        self.systemtest_path = (
+            pathlib.Path(str(os.getenv("JPSCORE_SOURCE_PATH"))) / "systemtest"
+        )
+
+
+@pytest.fixture
+def env():
+    return Environment()
+
+
+class JpsCoreDriver:
+    def __init__(self, *, jpscore_path: pathlib.Path, working_directory: pathlib.Path):
+        self.working_directory = working_directory
+        self.jpscore_path = jpscore_path
         self.result = None
+        self.logfile = self.working_directory / "logfile.txt"
+        self.traj_file = self.working_directory / "results/traj.txt"
         # ... tbd
 
         pass
 
-    def run(self, working_directory: pathlib.Path, test_directory: pathlib.Path):
+    def run(self):
+        with open(self.logfile, "w") as logfile:
+            self.result = subprocess.run(
+                [self.jpscore_path, "inifile.xml"],
+                cwd=self.working_directory,
+                stdout=logfile,
+                stderr=subprocess.STDOUT,
+            )
 
-        self._setupTestDir(working_directory, test_directory)
-
-        self.result = subprocess.run(
-            [self.jpscore_path, "inifile.xml"],
-            cwd=str(working_directory / test_directory),
-            capture_output=True,
-            text=True,
-        )
-
-        # checkReturnCode?
-
-        # TODO write log to file here?
-
-        return
-
-    # Creates a test directory where the test is executed and results are written
-    # This test directory is temporary if tmp_path is used as parameter
-    def _setupTestDir(
-        self, working_directory: pathlib.Path, test_directory: pathlib.Path
-    ):
-
-        # TODO check src != dest
-        src_dir = self.system_test_dir / test_directory
-        dst_dir = working_directory / test_directory
-
-        # creat new test directory in directory of execution
-        subprocess.run(["mkdir -p " + str(dst_dir)], shell=True)
-        # TODO check result?
-
-        # copy all xml files to temporary test directory
-        # TODO maybe this should be realized with a subprocess as well?
-        for basename in os.listdir(src_dir):
-            if basename.endswith(".xml"):
-                pathname = os.path.join(src_dir, basename)
-                if os.path.isfile(pathname):
-                    shutil.copy2(pathname, dst_dir)
+        self.result.check_returncode()
 
     def logfile(self):
-        return self.result.stdout
+        return self.logfile
 
     def trajfile(self):
-        pass
-
-    def stats(self):
-        pass
-
-    def systemTestDir(self) -> pathlib.Path:
-        return self.system_test_dir
+        return self.traj_file
 
 
-@pytest.fixture()
-def jpscore_driver():
-    # TODO do we really need a fixture?
-    return JpsCoreDriver()
+# Creates a test directory where the test is executed and results are written
+# This test directory is temporary if tmp_path is used as parameter
+def copyAllFiles(*, src: pathlib.Path, dest: pathlib.Path):
+    if src == dest:
+        return
 
-
-def checkReturnCode():
-    # needed?
-    pass
+    # copy all xml files to temporary test directory
+    for filename in os.listdir(src):
+        pathname = src / filename
+        if os.path.isfile(pathname):
+            shutil.copy2(pathname, dest)
 
 
 def checkOutputFilesCreated(working_directory: pathlib.Path):
@@ -84,16 +67,15 @@ def checkOutputFilesCreated(working_directory: pathlib.Path):
     pass
 
 
-def getFileTextDiff(expected_file: pathlib.Path, new_file: pathlib.Path):
-
-    with open(expected_file) as expected_data:
+def getFileTextDiff(*, expected: pathlib.Path, actual: pathlib.Path):
+    with open(expected) as expected_data:
         expected_text = expected_data.readlines()
-    with open(new_file) as new_data:
+    with open(actual) as new_data:
         new_text = new_data.readlines()
 
     diff_text = "".join(
         difflib.context_diff(
-            expected_text, new_text, fromfile=str(expected_file), tofile=str(new_file)
+            expected_text, new_text, fromfile=str(expected), tofile=str(actual)
         )
     )
 
@@ -108,18 +90,22 @@ def getFileTextDiff(expected_file: pathlib.Path, new_file: pathlib.Path):
         (pathlib.Path("reference_tests/RT02_corridor_velocity_global-shortest/")),
     ],
 )
-def test_reference_data(tmp_path, jpscore_driver, test_directory: pathlib.Path):
+def test_reference_data(tmp_path, env, test_directory):
+    copyAllFiles(src=env.systemtest_path / test_directory / "input", dest=tmp_path)
 
-    jpscore_driver.run(tmp_path, test_directory)
+    jpscore_driver = JpsCoreDriver(
+        jpscore_path=env.jpscore_path, working_directory=tmp_path
+    )
+    jpscore_driver.run()
     # checkOutputFilesCreated(test_directory)
 
     # check if there is no diff between expected and new output
     # TODO check for different OS
+    expected = env.systemtest_path / test_directory / "expected_linux/traj.txt"
+    actual = tmp_path / "results/traj.txt"
     diff = getFileTextDiff(
-        pathlib.Path(
-            jpscore_driver.systemTestDir(), test_directory, "output_expected/traj.txt"
-        ),
-        pathlib.Path(tmp_path, test_directory, "results/traj.txt"),
+        expected=expected,
+        actual=actual,
     )
     # diff must be empty
     assert diff == ""
