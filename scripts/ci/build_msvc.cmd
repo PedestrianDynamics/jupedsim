007085fd599b1e316b8b2bcb8e0fe86fd40783a3
Ozaq <ozaq@posteo.de>
2021-12-08 16:18:14 +0100

    Fixup pytest wrapper

--- scripts/ci/build_msvc.cmd
+++ scripts/ci/build_msvc.cmd
@@ -2,5 +2,5 @@ mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=C:/Users/admin/vcpkg/scripts/buildsystems/vcpkg.cmake -DBUILD_TESTS=ON
 cmake --build . --config Release
-cmake --build . --config Release -t tests 
+cmake --build . --config Release -t unittests 
 cmake --build . --config Release -t package
