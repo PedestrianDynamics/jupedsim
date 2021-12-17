dcc080dde52c470264e58b1cef36e1ea717ee8b4
Ozaq <ozaq@posteo.de>
2021-12-17 22:35:40 +0100

    Enable venv env on mac

--- scripts/ci/build_apple_clang.sh
+++ scripts/ci/build_apple_clang.sh
@@ -2,6 +2,9 @@ set -e
 numcpus=$(sysctl -n hw.logicalcpu)
 mkdir build && cd build
 ./../scripts/setup-deps.sh
+python3 -m venv python-env
+source python-env/bin/activate
+pip install -r ../requirements.txt
 cmake .. -DCMAKE_PREFIX_PATH=$(pwd)/deps -DBUILD_TESTS=ON
 cmake --build . -- -j ${numcpus}
 cmake --build . -t tests -- -j ${numcpus}
