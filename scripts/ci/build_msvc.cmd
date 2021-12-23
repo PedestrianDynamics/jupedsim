b79a50faf95b68659e657fd72d4e0e8c7e0dc952
Ozaq <ozaq@posteo.de>
2021-12-23 13:37:27 +0100

    Add rust to docker image

--- scripts/ci/build_msvc.cmd
+++ scripts/ci/build_msvc.cmd
@@ -1,4 +1,5 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+rustup default stable
 mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=C:/jpscore-deps/scripts/buildsystems/vcpkg.cmake -DBUILD_TESTS=ON
