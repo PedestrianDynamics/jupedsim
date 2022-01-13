192cd550b985d6268fb76a4fb5dc19356ac86467
schroedtert <tobias.schroedter@rwth-aachen.de>
2022-01-13 01:34:25 -0800

    Fix windows installer creation (#131)
    Co-authored-by: Ozaq <Ozaq@posteo.de>

--- jpsvis/cmake/install_helper.cmake
+++ jpsvis/cmake/install_helper.cmake
@@ -15,7 +15,6 @@ macro(write_package_information)
     set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_SOURCE_DIR}/README.md")
     set(CPACK_PACKAGE_HOMEPAGE_URL "http://jupedsim.org")
     set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/README.md")
-    set(CPACK_PACKAGE_EXECUTABLES "jpsvis")
     set(CPACK_MONOLITHIC_INSTALL TRUE)
     set(CPACK_CREATE_DESKTOP_LINKS jpsvis)
     set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE")
@@ -70,6 +69,7 @@ macro(write_package_information_macos)
     SET(MACOSX_BUNDLE_SHORT_VERSION_STRING "${PROJECT_VERSION}")
     SET(MACOSX_GUI_COPYRIGHT "Copyright (c) 2015-2021 Forschungszentrum Juelich. All rights reserved.")
     SET(MACOSX_BUNDLE_GUI_IDENTIFIER "www.jupedsim.org")
+    set(CPACK_PACKAGE_EXECUTABLES "jpsvis;JPSVis")
 endmacro()
 
 macro(write_package_content_macos)
@@ -108,49 +108,119 @@ macro(write_package_content_macos)
 
 endmacro()
 
-###############################################################################
-# WINDOWS
-###############################################################################
-function(cpack_write_windows_package_information)
-    set(CPACK_GENERATOR "NSIS" CACHE STRING "Generator used by CPack")
+################################################################################
+# Windows Installer
+################################################################################
+macro(write_package_information_win)
+  set(CPACK_GENERATOR "NSIS" CACHE STRING "Generator used by CPack")
+  set(CPACK_NSIS_HELP_LINK "www.jupedsim.org")
+  set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/forms/jpsvis.ico")
+  set(CPACK_NSIS_DISPLAY_NAME "JPSvis")
+  set(CPACK_NSIS_PACKAGE_NAME "JPSvis")
+  set(CPACK_NSIS_INSTALLED_ICON_NAME "${CMAKE_SOURCE_DIR}/forms/jpsvis.ico")
+  set(CPACK_NSIS_URL_INFO_ABOUT ${CPACK_NSIS_HELP_LINK})
+  set(CPACK_NSIS_MENU_LINKS
+    "bin/jpsvis.exe"
+    "jpsvis")
+  set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
+  set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS
+	  "CreateShortCut '$DESKTOP\\\\JPSvis.lnk' '$INSTDIR\\\\bin\\\\jpsvis.exe'"
+  )
+  set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS
+	  "Delete '$DESKTOP\\\\JPSvis.lnk'"
+  )
+endmacro()
 
-    set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/forms/icons/jpsvis.ico")
-    set(CPACK_NSIS_MODIFY_PATH ON)
-    set(CPACK_NSIS_DISPLAY_NAME "JPSvis")
-    set(CPACK_NSIS_PACKAGE_NAME "JPSvis")
-    set(CPACK_NSIS_INSTALLED_ICON_NAME "${CMAKE_SOURCE_DIR}/forms/icons/jpsvis.ico")
-    set(CPACK_NSIS_HELP_LINK "www.jupedsim.org")
-    set(CPACK_NSIS_URL_INFO_ABOUT ${CPACK_NSIS_HELP_LINK})
-    set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
-endfunction()
-
-
-function(cpack_create_windows_package)
+macro(write_package_content_win)
     include(InstallRequiredSystemLibraries)
     include(GNUInstallDirs)
+    install(TARGETS jpsvis)
+    install(FILES
+        $<TARGET_FILE_DIR:jpsvis>/brotlicommon.dll
+        $<TARGET_FILE_DIR:jpsvis>/brotlidec.dll
+        $<TARGET_FILE_DIR:jpsvis>/bz2.dll
+        $<TARGET_FILE_DIR:jpsvis>/freetype.dll
+        $<TARGET_FILE_DIR:jpsvis>/glew32.dll
+        $<TARGET_FILE_DIR:jpsvis>/harfbuzz.dll
+        $<TARGET_FILE_DIR:jpsvis>/icuuc69.dll
+        $<TARGET_FILE_DIR:jpsvis>/jpeg62.dll
+        $<TARGET_FILE_DIR:jpsvis>/libexpat.dll
+        $<TARGET_FILE_DIR:jpsvis>/libpng16.dll
+        $<TARGET_FILE_DIR:jpsvis>/lz4.dll
+        $<TARGET_FILE_DIR:jpsvis>/lzma.dll
+        $<TARGET_FILE_DIR:jpsvis>/pcre2-16.dll
+        $<TARGET_FILE_DIR:jpsvis>/pugixml.dll
+        $<TARGET_FILE_DIR:jpsvis>/Qt5Core.dll
+        $<TARGET_FILE_DIR:jpsvis>/Qt5Gui.dll
+        $<TARGET_FILE_DIR:jpsvis>/Qt5Widgets.dll
+        $<TARGET_FILE_DIR:jpsvis>/Qt5Xml.dll
+        $<TARGET_FILE_DIR:jpsvis>/tiff.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonColor-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonComputationalGeometry-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonCore-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonDataModel-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonExecutionModel-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonMath-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonMisc-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonSystem-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkCommonTransforms-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkDICOMParser-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersCore-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersExtraction-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersGeneral-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersGeometry-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersHybrid-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersModeling-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersSources-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersStatistics-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkFiltersTexture-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkGUISupportQt-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkImagingColor-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkImagingCore-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkImagingFourier-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkImagingGeneral-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkImagingHybrid-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkImagingSources-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkImagingSources-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkInteractionStyle-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkInteractionWidgets-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkIOCore-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkIOImage-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkIOLegacy-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkIOXML-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkIOXMLParser-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkloguru-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkmetaio-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkParallelCore-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkParallelDIY-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkRenderingAnnotation-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkRenderingCore-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkRenderingFreeType-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkRenderingLabel-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkRenderingOpenGL2-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtkRenderingUI-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/vtksys-9.0.dll
+        $<TARGET_FILE_DIR:jpsvis>/zlib1.dll
+        $<TARGET_FILE_DIR:jpsvis>/zstd.dll
+        $<TARGET_FILE_DIR:jpsvis>/icuin69.dll
+        $<TARGET_FILE_DIR:jpsvis>/icudt69.dll
+        TYPE BIN
+    )
+    install(DIRECTORY
+        $<TARGET_FILE_DIR:jpsvis>/plugins
+        TYPE BIN
+    )
+endmacro()
 
-    install(TARGETS petrack)
+################################################################################
+# Deb package
+################################################################################
+macro(write_package_information_unix)
+    message(STATUS "Package generation - Deb")
+    message(STATUS "not yet implemented")
+endmacro()
 
-    # install Qwt and OpenCV
-    install(CODE "
-        include(BundleUtilities)
-        fixup_bundle(\"\${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR}/jpsvis${CMAKE_EXECUTABLE_SUFFIX}\"  \"\" \"\")
-    ")
-
-    get_target_property(mocExe Qt5::moc IMPORTED_LOCATION)
-    get_filename_component(qtBinDir "${mocExe}" DIRECTORY)
-
-    find_program(DEPLOYQT_EXECUTABLE windeployqt PATHS "${qtBinDir}" NO_DEFAULT_PATH)
-endfunction()
-
-
-###############################################################################
-# UNIX
-###############################################################################
-function (write_package_information_unix)
-    message(STATUS "Package generation - Unix - not yet implemented")
-endfunction()
-
-function (write_package_content_unix)
-    message(STATUS "Cpack write configs - Unix - not yet implemented")
-endfunction()
+macro(write_package_content_unix)
+    message(STATUS "Package generation - Deb")
+    message(STATUS "not yet implemented")
+endmacro()
