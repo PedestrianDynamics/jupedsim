10e5d397a59d5c81200e70b9cb91e2553afb4019
Ozaq <ozaq@posteo.de>
2022-01-14 17:25:28 +0100

    wip

--- scripts/ci/build_msvc.cmd
+++ scripts/ci/build_msvc.cmd
@@ -1,6 +1,6 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_PREFIX_PATH=C:/vcpkg/scripts/installed/x64-windows-release -DBUILD_TESTS=ON
+cmake .. -DCMAKE_PREFIX_PATH=C:/vcpkg/installed/x64-windows-release -DBUILD_TESTS=ON
 cmake --build . --config Release -j
 cmake --build . --config Release -t unittests 
 cmake --build . --config Release -t package
