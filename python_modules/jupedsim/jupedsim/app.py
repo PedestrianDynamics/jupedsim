fb0f4c352c80367e206c5b953958ae9cab4f7811
ChristianHirt <c.hirt@fz-juelich.de>
2022-09-08 13:37:12 +0200

    all parameter changeable in main, place_alleverywhere implemented in main

--- python_modules/jupedsim/jupedsim/app.py
+++ python_modules/jupedsim/jupedsim/app.py
@@ -43,27 +43,56 @@ def show_points(polygon, samples, radius, obstacles=None):
     plt.axis('equal')
     st.pyplot(fig)
 
+
 def main():
-    polygon = [(0, 0), (10, 0), (10, 10), (0, 10)]
-    radius = 0.3
+    default_polygon = [(0.0, 0.0), (10.0, 0.0), (10.0, 10.0), (0.0, 10.0)]
+    agents = density = 0
+
+    with st.sidebar:
+        a_r = st.slider("space among agents", 0.1, 2.0, value=0.3)
+        w_d = st.slider("space between agents and walls", 0.1, 2.0, value=0.3)
+        col1, col2 = st.columns(2)
+        corners = st.number_input("number of corner points in polygon", 3, value=4)
+        x_values, y_values = [], []
+        with col1:
+            for i in range(corners):
+                x_values.append(st.number_input(f"x value {i+1}",
+                                                value=default_polygon[i][0] if i < len(default_polygon) else 0))
+        with col2:
+            for i in range(corners):
+                y_values.append(st.number_input(f"y value {i+1}",
+                                                value=default_polygon[i][1] if i < len(default_polygon) else 0))
+
+    polygon = []
+    for x, y in zip(x_values, y_values):
+        polygon.append((x, y))
     area = distribution.area_of_polygon(polygon)
-    st.text('below should be a button')
-    style = st.radio("how to choose number of agents?", ("density", "number"))
-    if style == "density":
-        agents = None
-        density = st.slider("persons / m²", 0.1, 7.5)
-    elif style == "number":
-        agents = st.slider("agents", 1, round(area*5))
-        density = None
-    else:
-        agents = density = 0
-    button_clicked = st.button('distribute agents')
+    distribution_type = st.radio("how to destribute agents?", ("place random","place everywhere"))
 
-    if button_clicked:
-        samples = distribution.create_random_points(polygon, agents, radius, radius, density=density)
-        st.text('below should be a plot')
-        show_points(polygon, samples, radius)
+    if distribution_type == "place random":
+        style = st.radio("how to choose number of agents?", ("density", "number"))
+        if style == "density":
+            agents = None
+            density = st.slider("persons / m²", 0.1, 7.5)
+        elif style == "number":
+            agents = st.slider("agents", 1, round(area * 5))
+            density = None
+
+        button_clicked = st.button('distribute agents')
+
+        if button_clicked:
+            samples = distribution.create_random_points(polygon, agents, a_r, w_d, density=density)
+            st.text('below should be a plot')
+            show_points(polygon, samples, a_r)
+
+    if distribution_type == "place everywhere":
+        button_clicked = st.button('distribute agents')
+
+        if button_clicked:
+            samples = distribution.create_points_everywhere(polygon, a_r, w_d)
+            st.text('below should be a plot')
+            show_points(polygon, samples, a_r)
 
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     main()
