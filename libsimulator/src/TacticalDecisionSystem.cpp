4a70c39b1ef6617a32488dbe9b52e03eff838d37
Ozaq <ozaq@posteo.de>
2022-09-21 10:32:32 +0200

    Fix bug #1027
    Removes code that attepmted to correct agents try to move to close to
    the navigation mesh boundary. This code had been superseeded by a
    differnt approach but had not been removed.

--- libsimulator/src/TacticalDecisionSystem.cpp
+++ libsimulator/src/TacticalDecisionSystem.cpp
@@ -14,11 +14,5 @@ void TacticalDecisionSystem::Run(
         const auto dest = agent->waypoint;
         const auto waypoints = routingEngine.ComputeWaypoint(agent->GetPos(), dest);
         agent->destination = waypoints[1];
-        if(waypoints.size() > 2) {
-            const auto vecA = (waypoints[0] - waypoints[1]).Normalized();
-            const auto vecB = (waypoints[2] - waypoints[1]).Normalized();
-            const auto adjustment = (vecA + vecB).Normalized() * -1.2;
-            agent->destination = agent->destination + adjustment;
-        }
     }
 }
