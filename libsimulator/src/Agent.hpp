0ca831b5f7819edaf41679a31d5b60271566f58d
Ozaq <ozaq@posteo.de>
2022-09-27 08:50:42 +0200

    VelocityModel agent radius now explicit field

--- libsimulator/src/Agent.hpp
+++ libsimulator/src/Agent.hpp
@@ -29,11 +29,19 @@ public:
     Point pos;
     Point orientation;
 
+    // TODO(kkratz): this cannot be changed by the model, this is an external parameter -> move to
+    // parameter profile
     /// Desired speed of agent
     double v0;
 
-    // GCFM Only fields
-    Ellipse ellipse{};
+    // TODO(kkratz): this cannot be changed by the model, this is an external parameter -> move to
+    // parameter profile VelocityModel ONLY fields
+    double radius; // constant
+
+    // GCFMModel ONLY fields
+    // TODO(kkratz): this cannot be changed by the model, this is an external parameter -> move to
+    // parameter profile
+    Ellipse ellipse{}; // constant
     // Guaranteed to be a unit vector
     double speed;
 
