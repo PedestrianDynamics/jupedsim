4d0e6aed808e2ceddc2a2ad4e187ae740d53cf7a
ChristianHirt <c.hirt@fz-juelich.de>
2022-10-12 15:09:34 +0200

    added information on distributions, grid class docstring added

--- python_modules/jupedsim/README.md
+++ python_modules/jupedsim/README.md
@@ -1,4 +1,13 @@
 ## Agent Distribution
+* The Distribution allowes to randomly distribute agents inside a Polygon
+
+* Agents will keep distance to other agents as well as to the polygon borders
+
+* There are four methods to distribute agents:
+
+	* distribute by density or number
+	* distribute in circle segments by number or density
+	
 * Constraints
 	* When distributing to two polygons, each polygon must be distributed once.
 	* The polygons must not overlap
@@ -6,6 +15,8 @@
 
 ## Testing
 
-from ```jpscore/python_modules/jupedsim``` you can call ```python -m pytest```
+* Test files are located in ```jpscore/python_modules/jupedsim/tests```
 
-to test the Distribution visually call ```python -m streamlit run jupedsim/app.py``` from ```jpscore/python_modules/jupedsim```
+* to test pytest files you can call ```python -m pytest``` from ```jpscore/python_modules/jupedsim``` 
+
+* to test the distribution functions visually call ```python -m streamlit run jupedsim/app.py``` from ```jpscore/python_modules/jupedsim```
--- python_modules/jupedsim/jupedsim/Grid.py
+++ python_modules/jupedsim/jupedsim/Grid.py
@@ -3,16 +3,21 @@ from math import sqrt
 
 
 class Grid:
+    """Class to save points and check for neighbours within a radius
+
+    box : an Axis Aligned Bounding Box where the Grid will be able to save points
+    agent_radius : radius in which points are searched for
+    """
     def __init__(self, box, agent_radius):
         self.box = box
         self.a_r = agent_radius
         width, height = box[1][0] - box[0][0], box[1][1] - box[0][1]
         # Cell side length
-        self.c_s_l = agent_radius / np.sqrt(2)
+        self._c_s_l = agent_radius / np.sqrt(2)
         # Number of cells in the x- and y-directions of the grid
-        self.nx, self.ny = int(width / self.c_s_l) + 1, int(height / self.c_s_l) + 1
+        self._nx, self._ny = int(width / self._c_s_l) + 1, int(height / self._c_s_l) + 1
         # A list of coordinates in the grid of cells
-        self.coords_list = [(ix, iy) for ix in range(self.nx) for iy in range(self.ny)]
+        self.coords_list = [(ix, iy) for ix in range(self._nx) for iy in range(self._ny)]
         # Initialize the dictionary of cells: each key is a cell's coordinates, the
         # corresponding value is the index of that cell's point's coordinates in the
         # samples list (or None if the cell is empty).
@@ -31,7 +36,7 @@ class Grid:
     def get_cell_coords(self, pt):
         """ Get the coordinates of the cell that pt = (x,y) falls in.
             box is bounding box containing the minimal/maximal x and y values"""
-        return int((pt[0] - self.box[0][0]) // self.c_s_l), int((pt[1] - self.box[0][1]) // self.c_s_l)
+        return int((pt[0] - self.box[0][0]) // self._c_s_l), int((pt[1] - self.box[0][1]) // self._c_s_l)
 
     def no_neighbours_in_distance(self, pt):
         coords = self.get_cell_coords(pt)
@@ -44,8 +49,8 @@ class Grid:
                 (-1, 2), (0, 2), (1, 2), (0, 0)]
         for dx, dy in dxdy:
             neighbour_coords = coords[0] + dx, coords[1] + dy
-            if not (0 <= neighbour_coords[0] < self.nx and
-                    0 <= neighbour_coords[1] < self.ny):
+            if not (0 <= neighbour_coords[0] < self._nx and
+                    0 <= neighbour_coords[1] < self._ny):
                 # Points are not on the grid
                 continue
             neighbour = self.cells[neighbour_coords]
