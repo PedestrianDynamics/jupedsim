c78096e5ac7970442875e6632e1ae28135f65576
Christian Hirt <c.hirt@fz-juelich.de>
2023-03-31 09:54:13 +0200

    prints polygon as list of points for each subroom

--- scripts/read_geometry.py
+++ scripts/read_geometry.py
@@ -1,30 +1,35 @@
 import xml.etree.ElementTree as ET
 
-
 if __name__ == "__main__":
     tree = ET.parse('correct_aknz_geo_arrival.xml')
     root = tree.getroot()
     subrooms = root.findall('.//subroom')
 
+    
     for subroom in subrooms:
         print('Subroom:', subroom.get('id'))
+        room_obstacles = []
+        room_polygon = []
         polygons = subroom.findall('polygon')
         obstacles = subroom.findall('obstacle')
         for obstacle in obstacles:
-            print('Obstacle:', obstacle.get('caption'))
-            polygons = subroom.findall('polygon')
-            for polygon in polygons:
-                print('Obstacle-Polygon:', polygon.get('caption'))
+            temp_obstacle = []
+            obstacle_polygons = obstacle.findall('polygon')
+            for polygon in obstacle_polygons:
                 vertices = polygon.findall('vertex')
                 for vertex in vertices:
-                    x = vertex.get('px')
-                    y = vertex.get('py')
-                    print('  Vertex:', x, y)
+                    point = (float(vertex.get('px')), float(vertex.get('py')))
+                    if point not in temp_obstacle:
+                        temp_obstacle.append(point)
+
+            room_obstacles.append(temp_obstacle)
 
         for polygon in polygons:
-            print('Polygon:', polygon.get('caption'))
             vertices = polygon.findall('vertex')
             for vertex in vertices:
-                x = vertex.get('px')
-                y = vertex.get('py')
-                print('  Vertex:', x, y)
+                point = (float(vertex.get('px')), float(vertex.get('py')))
+                if point not in room_polygon:
+                    room_polygon.append(point)
+
+        print(room_polygon)
+        print(room_obstacles)
