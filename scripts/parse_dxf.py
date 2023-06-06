d652b10cd82975b11f870744566278be40170671
Christian Hirt <c.hirt@fz-juelich.de>
2023-06-06 13:24:45 +0200

    parsing worked

--- scripts/parse_dxf.py
+++ scripts/parse_dxf.py
@@ -1,5 +1,6 @@
 import ezdxf
-from shapely import LineString, Polygon, polygonize
+import shapely
+from shapely import LineString, Polygon, polygonize, MultiPolygon, to_wkt, unary_union
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
@@ -11,17 +12,28 @@ def plot_polygon(polygon):
     plt.show()
 
 
+
 def line_to_linestring(line):
     start_point = entity.dxf.start
     end_point = entity.dxf.end
-    return [(start_point[0], start_point[1]), (end_point[0], end_point[1])]
+    return LineString([(start_point[0], start_point[1]), (end_point[0], end_point[1])])
+
 
 def polyline_to_linestring(polyline):
     points = []
     for point in polyline.get_points():
-        print(point)
         points.append([point[0], point[1]])
-    return points
+    return LineString(points)
+
+
+def polyline_to_polygon(polyline):
+    points = []
+    for point in polyline.get_points():
+        points.append([point[0], point[1]])
+    if len(points) < 3:
+        print(points)
+        return []
+    return Polygon(points)
 
 
 def put_line_together(lines):
@@ -46,7 +58,7 @@ def put_line_together(lines):
                 break
             elif resembles(line[-1], curr_pt):
                 # first point of line matches
-                for i in reversed(range(0, len(line)-1)):
+                for i in reversed(range(0, len(line) - 1)):
                     # add all points except matching one
                     polygon.append(line[i])
                 curr_pt = line[0]
@@ -61,17 +73,25 @@ def put_line_together(lines):
 def resembles(pt1, pt2, epsilon=0.01):
     diff_x = abs(pt1[0] - pt2[0])
     diff_y = abs(pt1[1] - pt2[1])
-    return  diff_x < epsilon and diff_y < epsilon
+    return diff_x < epsilon and diff_y < epsilon
 
+
+def plot_lines(lines):
+    fig, ax = plt.subplots()
+    for line in lines:
+        x_coords, y_coords = zip(*line)
+        plt.plot(x_coords, y_coords)
+    ax.autoscale()
+    plt.show()
 
 def plot_line_strings(lines):
     # Create a new figure and axis object
     fig, ax = plt.subplots()
 
-    # Iterate over the LineString objects
-    for line in lines:
-        x_coords, y_coords = zip(*line)
-        plt.plot(x_coords, y_coords)
+    for line_string in lines:
+        # Extrahieren der Koordinaten des LineString-Objekts
+        x, y = line_string.xy
+        ax.plot(x, y)
 
     # Adjust the axis limits
     ax.autoscale()
@@ -82,6 +102,7 @@ if __name__ == "__main__":
     # Path to the DXF file
     file_path = "./SiB2023_entrance_jupedsim.dxf"
     hole_lines = []
+    holes = []
     outer_lines = []
     # Open the DXF file
     doc = ezdxf.readfile(file_path)
@@ -98,16 +119,42 @@ if __name__ == "__main__":
                 outer_lines.append(line_to_linestring(entity))
         elif entity.dxftype() == "LWPOLYLINE":
             if entity.dxf.layer == "jupedsim_holes":
-                hole_lines.append(polyline_to_linestring(entity))
+                holes.append(polyline_to_polygon(entity))
             elif entity.dxf.layer == "jupedsim_walkable_area":
                 outer_lines.append(polyline_to_linestring(entity))
-        else:
+        elif entity.dxftype() != "INSERT":
             print("entity type:", entity.dxftype())
             print()
-    plot_line_strings(hole_lines)
-    plot_line_strings(outer_lines)
+    # plot the parsed lines from the dxf file
     for line in outer_lines:
         print(line)
-    outer_polygon = put_line_together(outer_lines)
-    print(outer_polygon)
-    plot_line_strings([outer_polygon])
+
+    plot_line_strings(outer_lines)
+
+    # create a polygon from all outer lines (only works if outer_lines is one polyline)
+    outer_polygon = Polygon(outer_lines[0].coords)
+    # create polygon with own method and cast into a polygon
+    # outer_polygon = put_line_together(outer_lines)
+    # outer_polygon = Polygon(outer_polygon)
+    print("the polygon: ", outer_polygon)
+    # plot the created polygon
+    plot_polygon(outer_polygon)
+    # extract all holes from the polygon
+
+
+    # create new Polygon with holes
+    holes_ext = [hole.exterior for hole in holes]
+    merged_polygon = Polygon(outer_polygon.exterior, holes_ext)
+    plot_polygon(merged_polygon)
+    # difference between outer and holes
+    merged_polygon = outer_polygon
+    holes = polygonize(holes)
+    merged_polygon = merged_polygon.difference(holes)
+    plot_polygon(merged_polygon)
+    """
+    # for hole in holes:
+       #  merged_polygon = merged_polygon.difference(hole)
+    # plot the polygon containing holes
+    plot_polygon(merged_polygon)
+    """
+    # now cast to wkt and write into a file
