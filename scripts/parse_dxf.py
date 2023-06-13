58b132336788dedcbea12e62a82d23f1ec6279c9
Christian Hirt <c.hirt@fz-juelich.de>
2023-06-13 13:33:50 +0200

    script parses dxf and creates wkt

--- scripts/parse_dxf.py
+++ scripts/parse_dxf.py
@@ -1,6 +1,5 @@
 import ezdxf
-import shapely
-from shapely import LineString, Polygon, polygonize, MultiPolygon, to_wkt, unary_union, GeometryCollection
+from shapely import LineString, Polygon, polygonize, to_wkt, GeometryCollection
 import matplotlib.pyplot as plt
 import geopandas as gpd
 
@@ -53,7 +52,7 @@ def plot_line_strings(lines):
 
 if __name__ == "__main__":
     # Path to the DXF file
-    file_path = "./SiB2023_entrance_jupedsim.dxf"
+    file_path = "SiB2023_entrance_jupedsim.dxf"
     hole_lines = []
     holes = []
     outer_lines = []
@@ -66,24 +65,42 @@ if __name__ == "__main__":
     # Iterate over all entities in the model
     for entity in msp:
         if entity.dxftype() == "LWPOLYLINE":
-            if entity.dxf.layer == "jupedsim_holes":
+            if entity.dxf.layer == "jupedsim_holes" or entity.dxf.layer == "entranceGates":
                 holes.append(polyline_to_polygon(entity))
             elif entity.dxf.layer == "jupedsim_walkable_area":
                 outer_lines.append(polyline_to_linestring(entity))
         elif entity.dxftype() != "INSERT":
             print("entity type:", entity.dxftype())
-    # plot the parsed lines from the dxf file
-    plot_line_strings(outer_lines)
 
     # create a polygon from all outer lines (only works if outer_lines is one polyline)
     outer_polygon = Polygon(outer_lines[0].coords)
+    plot_polygon(outer_polygon)
+
+    # separate simple and not simple holes
+    simple_holes = []
+    other_holes = []
+    for hole in holes:
+        simple_holes.append(hole) if hole.is_simple else other_holes.append(hole)
+
+    if len(other_holes) > 0:
+        # do exception handling
+        print(f"there are {len(other_holes)} not simple polygons left")
+
 
     # create new Polygon with holes
-    holes = polygonize(holes)
-    merged_polygon = outer_polygon.difference(holes)
+    """ this does not work. unsure why
+    hole_ext = [hole.exterior for hole in simple_holes]
+    merged_polygon = Polygon(outer_polygon.boundary, holes=hole_ext)
+    """
+    simple_holes = polygonize(simple_holes)
+    merged_polygon = outer_polygon.difference(simple_holes)
+    # this however does work
+
+    # plot final Polygon
     plot_polygon(merged_polygon)
     # now cast to wkt and write into a file
     out_file = "entrance_jupedsim.wkt"
     geomery_collection = GeometryCollection(merged_polygon)
-    with open(out_file, 'a') as out:
+    with open(out_file, 'w') as out:
         out.write(to_wkt(geomery_collection, rounding_precision=-1))
+    print("file written into out_file")
