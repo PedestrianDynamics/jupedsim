75e4c92ab71dd3310c47098824382d5419ecc9e7
schroedtert <t.schroedter@fz-juelich.de>
2023-07-26 07:31:56 +0200

    Remove Grid2D as it is not needed anymore

--- libsimulator/test/TestNeighborhoodSearch.cpp
+++ libsimulator/test/TestNeighborhoodSearch.cpp
@@ -8,32 +8,6 @@
 #include <iterator>
 #include <memory>
 
-// TEST(Grid2D, EmptyDefauneighborhood_searchorhood_searchtruction)
-//{
-//     Grid2D<int> grid;
-//
-//     ASSERT_EQ(grid.size(), 0);
-//     ASSERT_TRUE(grid.empty());
-// }
-//
-// TEST(Grid2D, Construction)
-//{
-//     std::vector<Grid2D<double>::Entry> values;
-//
-//     for(int i = 0; i < 100; ++i) {
-//         values.push_back({{100 - i, i}, {100.0 - i, static_cast<double>(i)}, 2.4});
-//     }
-//
-//     Grid2D<double> grid(values);
-//
-//     ASSERT_EQ(grid.size(), 100);
-//     ASSERT_FALSE(grid.empty());
-//
-//     auto it_pair = grid.get({99, 1});
-//     ASSERT_EQ(std::distance(it_pair.first(), it_pair.second()), 1);
-//     ASSERT_EQ(it_pair.first()->value, 2.4);
-// }
-
 template <typename T>
 struct ValueWithPos {
     Point pos{};
