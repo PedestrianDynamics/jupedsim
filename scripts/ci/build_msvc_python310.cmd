6d0f1efec6cc8fbfef85e30bb5c449f03dce2f57
Ozaq <ozaq@posteo.de>
2023-07-31 16:13:02 +0200

    Add windows builds for python 3.11/3.10

--- scripts/ci/build_msvc_python310.cmd
+++ scripts/ci/build_msvc_python310.cmd
@@ -1,6 +1,7 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-call C:\Python\Python311\python.exe -m venv C:\jupedsim-venv311
-call C:\jupedsim-venv311\Scripts\activate.bat
+rm -rf C:\jupedsim-venv310
+call C:\Python\Python310\python.exe -m venv C:\jupedsim-venv310
+call C:\jupedsim-venv310\Scripts\activate.bat
 mkdir build
 cd build
 cmake .. -DBUILD_TESTS=ON
---
+++ scripts/ci/build_msvc_python311.cmd
@@ -0,0 +1,9 @@
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+rm -rf C:\jupedsim-venv311
+call C:\Python\Python311\python.exe -m venv C:\jupedsim-venv311
+call C:\jupedsim-venv311\Scripts\activate.bat
+mkdir build
+cd build
+cmake .. -DBUILD_TESTS=ON
+cmake --build . --config Release
+cmake --build . --config Release --target unittests
