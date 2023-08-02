548822b5b90390bc99956c508fe1ca74e28ec26d
schroedtert <t.schroedter@fz-juelich.de>
2023-08-02 08:56:16 +0200

    Add readthedocs config file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,54 +5,50 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-project = 'JuPedSim'
+project = "JuPedSim"
 copyright = "2023, Forschungszentrum Jülich GmbH, IAS-7"
-author = 'Dev-Team'
+author = "Dev-Team"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.autodoc",
     "sphinx_design",
     "sphinx_copybutton",
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
     "sphinxcontrib.apidoc",
     "sphinx.ext.napoleon",
+    "autoapi.extension",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # automatic generation of api doc
+autoapi_dirs = [
+    "../../python_modules/jupedsim",
+]
+autoapi_root = "api"
+autoapi_options = [
+    "members",
+    "undoc-members",
+    # "inherited-members",
+    # "private-members",
+    "show-inheritance",
+    "show-module-summary",
+    # "special-members",
+    "imported-members",
+]
+autoapi_ignore = ["**/tests/**"]
+autoapi_member_order = ["groupwise"]
 
-apidoc_module_dir = "../../python_modules/jupedsim/jupedsim"
-apidoc_output_dir = "api"
-apidoc_excluded_paths = ["tests"]
-apidoc_separate_modules = True
-apidoc_toc_file = "index"
-apidoc_extra_args = ["--implicit-namespaces", "-d 10"]
-apidoc_module_first = True
-
-napoleon_include_init_with_doc = False
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = True
-napoleon_use_rtype = False
-napoleon_preprocess_types = False
-napoleon_type_aliases = None
-napoleon_attr_annotations = True
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
 
 html_logo = "_static/jupedsim.png"
 
