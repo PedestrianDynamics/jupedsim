b83993871d0b199bd025eb0f10b230c4ef6f325e
Ozaq <ozaq@posteo.de>
2023-09-12 08:27:54 +0200

    Build only once on windows but disabled unit tests

--- scripts/ci/build_msvc_python310.cmd
+++ scripts/ci/build_msvc_python310.cmd
@@ -2,12 +2,13 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 call C:\Python\Python310\python.exe -m venv venv310
 call venv310\Scripts\activate.bat
 pip install -r requirements.txt
-mkdir build
-set PYTHONPATH=%cd%\python_modules\jupedsim;%cd%\build\lib\Release
-cd build
-echo %PYTHONPATH%
-cmake .. -DBUILD_TESTS=ON -DCMAKE_UNITY_BUILD=ON
-cmake --build . --config Release
-cmake --build . --config Release --target unittests
-cd ..
+:: Build time in CI is currently too long to justify building twice just for unit tests
+::mkdir build
+::set PYTHONPATH=%cd%\python_modules\jupedsim;%cd%\build\lib\Release
+::cd build
+::echo %PYTHONPATH%
+::cmake .. -DBUILD_TESTS=ON -DCMAKE_UNITY_BUILD=ON
+::cmake --build . --config Release
+::cmake --build . --config Release --target unittests
+::cd ..
 python -m build -w
