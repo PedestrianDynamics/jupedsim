fa1e0a4463e83c48c383805fe6a289dbd37719bf
schroedtert <t.schroedter@fz-juelich.de>
2023-10-09 15:03:45 +0200

    Improve simulation doc

--- python_modules/jupedsim/jupedsim/simulation.py
+++ python_modules/jupedsim/jupedsim/simulation.py
@@ -26,12 +26,12 @@ from jupedsim.tracing import Trace
 
 
 class Simulation:
-    """Defines a simulation of pedestrian movement over a continous walkable area.
+    """Defines a simulation of pedestrian movement over a continuous walkable area.
 
     Movement of agents is described with Journeys, Stages and Transitions.
     Agents can be added and removed at will. The simulation processes one step
     at a time. No automatic stop condition exists. You can simulate multiple
-    disconnected walkable areas by instanciating multiple instances of
+    disconnected walkable areas by instantiating multiple instances of
     simulation.
     """
 
@@ -55,29 +55,30 @@ class Simulation:
         Arguments:
             model (VelocityModelParameters | GeneralizedCentrifugalForceModelParameters):
                 Defines the operational model used in the simulation.
-            geometry (str | shapely.GeometryCollection |
-                      shapely.Polygon | shapely.MultiPolygon |
-                      shapely.MultiPoint | list[tuple[float, float]]):
+            geometry (str | shapely.GeometryCollection | shapely.Polygon | shapely.MultiPolygon | shapely.MultiPoint | list[tuple[float, float]]):
                 Data to create the geometry out of. Data may be supplied as:
-                * list of 2d points describing the outer boundary, holes may be added with use of
-                    `excluded_areas` kw-argument
-                * GeometryCollection consiting only out of Polygons, MultiPolygons and MultiPoints
+
+                * list of 2d points describing the outer boundary, holes may be added with use of `excluded_area`s kw-argument
+
+                * GeometryCollection consisting only out of Polygons, MultiPolygons and MultiPoints
+
                 * MultiPolygon
+
                 * Polygon
-                * MultiPoint forming a "simple" polygon when points are interpreted as linear
-                    ring without repetition of the start/end point.
-                * str with a valid Well Known Text. In this format the same WKT types as mentioned
-                    for the shapely types are supported: GEOMETRYCOLLETION, MULTIPOLYGON, POLYGON,
-                    MULTIPOINT. The same restrictions as mentioned for the shapely types apply.
+
+                * MultiPoint forming a "simple" polygon when points are interpreted as linear ring without repetition of the start/end point.
+
+                * str with a valid Well Known Text. In this format the same WKT types as mentioned for the shapely types are supported: GEOMETRYCOLLETION, MULTIPOLYGON, POLYGON, MULTIPOINT. The same restrictions as mentioned for the shapely types apply.
+
             dt (float): Iteration step size in seconds. It is recommended to
                 leave this at its default value.
             trajectory_writer (TrajectoryWriter): Any object implementing the
-                TrajectoryWriter interface. Jupedim provides a writer that outputs trajectory data
+                TrajectoryWriter interface. JuPedSim provides a writer that outputs trajectory data
                 in a sqlite database. If you want other formats such as CSV you need to provide
                 your own custom implementation.
 
         Keyword Arguments:
-            excluded_areas (list[list[tuple(float, float)]]): descibes exclusions
+            excluded_areas (list[list[tuple(float, float)]]): describes exclusions
                 from the walkable area. Only use this argument if `geometry` was
                 provided as list[tuple[float, float]].
         """
@@ -116,9 +117,11 @@ class Simulation:
 
         Arguments:
             position (tuple[float, float]): Position of the waypoint
-            distance (float): Minimum distance required to reach this wayppint
+            distance (float): Minimum distance required to reach this waypoint
 
-        Returns (int): Id of the new stage.
+        Returns:
+            Id of the new stage.
+
         """
         return self._obj.add_waypoint_stage(position, distance)
 
@@ -130,7 +133,9 @@ class Simulation:
                 points of this queue. The first one in the list is the head of
                 the queue while the last one is the back of the queue.
 
-        Returns (int): If of the new stage.
+        Returns:
+            Id of the new stage.
+
         """
         return self._obj.add_queue_stage(positions)
 
