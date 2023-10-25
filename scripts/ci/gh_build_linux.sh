19ffa1575ce4dfc979b45dd37b1bfdbcc607d2ea
Ozaq <ozaq@posteo.de>
2023-10-25 13:06:01 +0200

    Remove unitybuild & unused scripts

--- scripts/ci/gh_build_linux.sh
+++ scripts/ci/gh_build_linux.sh
@@ -2,6 +2,6 @@ set -e
 echo Using ${CC} and ${CXX}
 numcpus=$(nproc)
 mkdir build && cd build
-cmake  .. -DBUILD_TESTS=ON -DWERROR=ON -DCMAKE_UNITY_BUILD=ON
+cmake  .. -DBUILD_TESTS=ON -DWERROR=ON
 cmake --build . -- -j ${numcpus} -- VERBOSE=1
 cmake --build . -t tests -- -j ${numcpus} -- VERBOSE=1
