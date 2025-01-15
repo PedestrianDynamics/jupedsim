7db6421d755b887c3745f428139c5faf20bcc66a
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2025-01-15 19:56:15 +0100

    clang format

--- libsimulator/src/Point.hpp
+++ libsimulator/src/Point.hpp
@@ -15,7 +15,7 @@ public:
     double y{};
 
 public:
-    Point(double x = 0, double y = 0) : x(x), y(y){};
+    Point(double x = 0, double y = 0) : x(x), y(y) {};
 
     /// Norm
     double Norm() const;
