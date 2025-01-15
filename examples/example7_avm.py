2444f891415a635ff2360e3efec8142be027fc51
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2025-01-15 20:53:25 +0100

    update example 7

--- examples/example7_avm.py
+++ examples/example7_avm.py
@@ -12,9 +12,7 @@ from shapely import Polygon
 
 def main():
     room1 = Polygon([(0, 0), (10, 0), (10, 10), (0, 10)])  # Room 1 (10m x 10m)
-    room2 = Polygon(
-        [(15, 0), (25, 0), (25, 10), (15, 10)]
-    )  # Room 2 (10m x 10m)
+    room2 = Polygon([(15, 0), (25, 0), (25, 10), (15, 10)])
     width = 0.8
     num_agents = 10
     corridor = Polygon(
@@ -26,8 +24,6 @@ def main():
         ]
     )
     geometry = room1.union(room2).union(corridor)
-    # geometry = shapely.GeometryCollection(shapely.box(0, -2.5, 50, 2.5))
-    # exit_polygon = shapely.box(48, -2.5, 50, 2.5)  #
     exit_polygon = Polygon([(24, 4.5), (25, 4.5), (25, 5.5), (24, 5.5)])
     trajectory_file = "example_7.sqlite"
     simulation = jps.Simulation(
@@ -44,7 +40,6 @@ def main():
 
     start_positions = jps.distribute_by_number(
         polygon=Polygon([(0, 0), (5, 0), (5, 10), (0, 10)]),
-        # shapely.box(0, -2.5, 5, 2.5),
         number_of_agents=num_agents,
         distance_to_agents=0.5,
         distance_to_polygon=0.2,
@@ -56,11 +51,8 @@ def main():
                 journey_id=journey_id,
                 stage_id=exit_id,
                 position=position,
-                radius=0.2,
-                # strength_neighbor_repulsion=8,
-                # range_neighbor_repulsion=0.1,
-                # strength_geometry_repulsion=5,
-                # range_geometry_repulsion=0.1,
+                anticipation_time=0.5,
+                reaction_time=0.3,
             )
         )
 
