473025d7ddef8a5f236a6f85236c6ca1183373e7
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2025-01-22 17:20:01 +0100

    remove unused include

--- libsimulator/src/Point.cpp
+++ libsimulator/src/Point.cpp
@@ -1,7 +1,6 @@
 // Copyright © 2012-2024 Forschungszentrum Jülich GmbH
 // SPDX-License-Identifier: LGPL-3.0-or-later
 #include "Point.hpp"
-#include <cassert>
 
 #include <Logger.hpp>
 #include <limits>
