939a91dd8d1b8cf56d454a8edebb94c7aed719e7
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2025-01-24 11:06:01 +0100

    Update more

--- librarytest/main.c
+++ librarytest/main.c
@@ -1,5 +1,4 @@
-/* Copyright © 2012-2024 Forschungszentrum Jülich GmbH */
-/* SPDX-License-Identifier: LGPL-3.0-or-later */
+// SPDX-License-Identifier: LGPL-3.0-or-later
 #include <jupedsim/jupedsim.h>
 
 #include <stddef.h>
