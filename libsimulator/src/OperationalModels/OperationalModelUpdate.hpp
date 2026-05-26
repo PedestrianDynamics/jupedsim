dac37a722878a0e2b97d545fb5784fdb052a6d17
Pit Steinbach <pit.steinbach@rwth-aachen.de>
2026-05-26 13:24:03 +0200

    Update Cmake setup for new folder setup

--- libsimulator/src/OperationalModels/OperationalModelUpdate.hpp
+++ libsimulator/src/OperationalModels/OperationalModelUpdate.hpp
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
-#include "AnticipationVelocityModel/AnticipationVelocityModelUpdate.hpp"
-#include "CollisionFreeSpeedModel/CollisionFreeSpeedModelUpdate.hpp"
-#include "CollisionFreeSpeedModelV2/CollisionFreeSpeedModelV2Update.hpp"
-#include "GeneralizedCentrifugalForceModel/GeneralizedCentrifugalForceModelUpdate.hpp"
-#include "SocialForceModel/SocialForceModelUpdate.hpp"
+#include "AnticipationVelocityModelUpdate.hpp"
+#include "CollisionFreeSpeedModelUpdate.hpp"
+#include "CollisionFreeSpeedModelV2Update.hpp"
+#include "CollisionFreeSpeedModelV3Update.hpp"
+#include "GeneralizedCentrifugalForceModelUpdate.hpp"
+#include "SocialForceModelUpdate.hpp"
+#include "WarpDriverModelUpdate.hpp"
 
 #include <variant>
 
@@ -11,5 +13,7 @@ using OperationalModelUpdate = std::variant<
     GeneralizedCentrifugalForceModelUpdate,
     CollisionFreeSpeedModelUpdate,
     CollisionFreeSpeedModelV2Update,
+    CollisionFreeSpeedModelV3Update,
     AnticipationVelocityModelUpdate,
-    SocialForceModelUpdate>;
+    SocialForceModelUpdate,
+    WarpDriverModelUpdate>;
