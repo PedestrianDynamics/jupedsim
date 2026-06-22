da7f6ead1277af5f41bccbcaf558ab90a508476e
Pit Steinbach <73583910+pitsteinbach@users.noreply.github.com>
2026-06-22 17:12:06 +0200

    Apply suggestions from code review
    Co-authored-by: ralfleibold <67294768+ralfleibold@users.noreply.github.com>

--- libsimulator/src/OperationalModels/CustomModel/CustomModelData.hpp
+++ libsimulator/src/OperationalModels/CustomModel/CustomModelData.hpp
@@ -6,7 +6,7 @@ class ICustomModelDataImpl
 {
 public:
     virtual ~ICustomModelDataImpl() {};
-    virtual std::string print() const = 0;
+    virtual std::string to_string() const = 0;
     virtual void apply_update(const CustomModelUpdate& upd) = 0;
 };
 
@@ -15,7 +15,7 @@ class CustomModelData
 public:
     explicit CustomModelData(std::shared_ptr<ICustomModelDataImpl> impl) : impl_(std::move(impl)) {}
     ~CustomModelData() = default;
-    std::string print() const
+    std::string to_string() const
     {
         if(impl_) {
             return impl_->print();
@@ -41,7 +41,6 @@ struct fmt::formatter<CustomModelData> : fmt::formatter<std::string> {
     template <typename FormatContext>
     auto format(const CustomModelData& data, FormatContext& ctx) const
     {
-        std::string repr = data.print();
-        return fmt::formatter<std::string>::format(repr, ctx);
+        return fmt::formatter<std::string>::format(data.to_string(), ctx);
     }
 };
