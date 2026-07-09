073e4d65beb8d80fc5283a4b7aa6a128a257eded
ralfleibold <67294768+ralfleibold@users.noreply.github.com>
2026-07-09 17:31:37 +0200

    Add 3D routing (#1607)
    * Add RoutingEngine3D interface. (The exact API might still change in the future.)
    
    * Add a new 3D routing implementation `SurfaceMeshShortestPathRoutingEngine` based on CGAL's `shortest_path_points_to_source_points()`.
    
    * Expose both to Python.
    
    * Add both C++ and Python tests to guard the implementation.
    
    * Add debug viewer based on trame that routes between 3D points.
    
    * Add examples/geometry/multi_level_u_stair.obj file as first 3D geometry: 2 floors with "U-stairs" in the middle.

--- python_bindings_jupedsim/bindings_jupedsim.cpp
+++ python_bindings_jupedsim/bindings_jupedsim.cpp
@@ -18,6 +18,7 @@ void init_social_force_model(py::module_& m);
 void init_warp_driver_model(py::module_& m);
 void init_geometry(py::module_& m);
 void init_routing(py::module_& m);
+void init_routing_3d(py::module_& m);
 void init_agent(py::module_& m);
 void init_transition(py::module_& m);
 void init_journey(py::module_& m);
@@ -46,6 +47,7 @@ PYBIND11_MODULE(py_jupedsim, m)
     init_agent_view(m);
     init_geometry(m);
     init_routing(m);
+    init_routing_3d(m);
     init_agent(m);
     init_transition(m);
     init_stage(m);
--- python_bindings_jupedsim/type_casters.hpp
+++ python_bindings_jupedsim/type_casters.hpp
@@ -3,6 +3,7 @@
 
 #include "Point.hpp"
 
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <pybind11/pybind11.h>
 
 // Type caster: According to https://pybind11.readthedocs.io/en/stable/advanced/cast/custom.html
@@ -30,6 +31,32 @@ struct type_caster<Point> {
     static handle cast(const Point& src, return_value_policy, handle)
     {
         return make_tuple(src.x, src.y).release();
+    }
+};
+
+// Point3D <-> tuple[float, float, float]
+// (using SurfaceKernel::Point_3 with EPICK kernel)
+template <>
+struct type_caster<CGAL::Exact_predicates_inexact_constructions_kernel::Point_3> {
+    using Point3D = CGAL::Exact_predicates_inexact_constructions_kernel::Point_3;
+    PYBIND11_TYPE_CASTER(Point3D, const_name("tuple[float, float, float]"));
+
+    bool load(handle src, bool)
+    {
+        if(!isinstance<sequence>(src)) {
+            return false;
+        }
+        auto seq = reinterpret_borrow<sequence>(src);
+        if(seq.size() != 3) {
+            return false;
+        }
+        value = Point3D(seq[0].cast<double>(), seq[1].cast<double>(), seq[2].cast<double>());
+        return true;
+    }
+
+    static handle cast(const Point3D& src, return_value_policy, handle)
+    {
+        return make_tuple(src.x(), src.y(), src.z()).release();
     }
 };
 } // namespace pybind11::detail
