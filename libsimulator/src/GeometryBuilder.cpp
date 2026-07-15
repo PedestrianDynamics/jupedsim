c0072e13c9cd457237223571e59e301192cb8039
Ozaq <ozaq@posteo.de>
2026-07-15 15:46:04 +0200

    Refactor 2D Walkable area construction
    Move code to Polygon from builder

--- libsimulator/src/GeometryBuilder.cpp
+++ libsimulator/src/GeometryBuilder.cpp
@@ -4,14 +4,10 @@
 #include "CfgCgal.hpp"
 #include "CollisionGeometry.hpp"
 #include "Point.hpp"
-#include "RoutingEngine.hpp"
-#include "SimulationError.hpp"
 
-#include <CGAL/Boolean_set_operations_2.h>
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
-#include <memory>
 #include <vector>
 
 GeometryBuilder& GeometryBuilder::AddAccessibleArea(const std::vector<Point>& lineLoop)
@@ -28,36 +24,8 @@ GeometryBuilder& GeometryBuilder::ExcludeFromAccessibleArea(const std::vector<Po
 
 CollisionGeometry GeometryBuilder::Build()
 {
-    const std::vector<Poly> accessibleListInput{
+    const std::vector<Poly> accessibleAreas{
         std::begin(_accessibleAreas), std::end(_accessibleAreas)};
-    PolyWithHolesList accessibleList{};
-
-    CGAL::join(
-        std::begin(accessibleListInput),
-        std::end(accessibleListInput),
-        std::back_inserter(accessibleList));
-
-    if(accessibleList.size() != 1) {
-        throw SimulationError("accessible area not connected");
-    }
-
-    auto accessibleArea = *accessibleList.begin();
-
-    const std::vector<Poly> exclusionsListInput{std::begin(_exclusions), std::end(_exclusions)};
-    PolyWithHolesList exclusionsList{};
-    CGAL::join(
-        std::begin(exclusionsListInput),
-        std::end(exclusionsListInput),
-        std::back_inserter(exclusionsList));
-
-    for(const auto& ex : exclusionsList) {
-        PolyWithHolesList res{};
-        CGAL::difference(accessibleArea, ex, std::back_inserter(res));
-        if(res.size() != 1) {
-            throw SimulationError("Exclusion splits accessibleArea");
-        }
-        accessibleArea = *res.begin();
-    }
-
-    return CollisionGeometry(accessibleArea);
+    const std::vector<Poly> exclusions{std::begin(_exclusions), std::end(_exclusions)};
+    return CollisionGeometry(CombinePolygons(accessibleAreas, exclusions));
 }
